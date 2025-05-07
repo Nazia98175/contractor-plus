bb2d990d4cbaac622304cd179ed67b9e5e70abeb
Tanu chaudhary <kaurtanu213@gmail.com>
2025-05-07 15:31:29 +0530

    working on hero

--- src/components/homepage/Hero.tsx
+++ src/components/homepage/Hero.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="flex items-center justify-between main-container">
+      <h1 className="main-heading text-white">
+        The only operating system for build & service contractors
+      </h1>
+      <p>
+        All the power of big software, none of the pain. One platform—not six—to
+        manage jobs, crews, customers, and growth.
+      </p>
+      <div className="flex"></div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;

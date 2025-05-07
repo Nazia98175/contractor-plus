848a866c8387c485c84eb30dcfa51f56b59f8703
Tanu chaudhary <kaurtanu213@gmail.com>
2025-05-07 17:37:18 +0530

    working on hero responsive

--- src/components/homepage/Hero.tsx
+++ src/components/homepage/Hero.tsx
@@ -4,6 +4,28 @@ import { CheckIcon, HeroBg } from "../common/Icons";
 const Hero = () => {
   return (
     <section className="relative overflow-hidden max-w-[1920px] mx-auto">
+      <div className="flex items-end lg:items-center main-container z-20 relative min-h-[700px] max-h-[892px] h-full">
+        <div className="max-w-[616px] w-full pt-20 pb-10 sm:space-y-6">
+          <h1 className="main-heading text-white mb-1.5">
+            The only operating system for build & service contractors
+          </h1>
+          <p className="text-decemberSky text-lg font-medium font-jakarta mb-4">
+            All the power of big software, none of the pain. One platform—not
+            six—to manage jobs, crews, customers, and growth.
+          </p>
+          <div className="flex gap-2.5 sm:flex-row flex-col items-center">
+            <button className="bg-red-linear h-10 primary-btn">
+              Create free account
+            </button>
+            <button className="flex gap-1.5 items-center font-myriad text-sm text-white cursor-pointer">
+              <CheckIcon />
+              No Credit Card Required
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* BACKGROUND  */}
       <img
         src="/images/webp/hero.webp"
         alt="Hero"
@@ -19,27 +41,6 @@ const Hero = () => {
         <HeroBg />
       </div>
       <div className="lg:hidden block bg-black-linear absolute inset-0 w-full h-full z-10"></div>
-
-      <section className="flex items-end lg:items-start main-container z-20 relative min-h-[700px] max-h-[892px] h-full">
-        <div className="max-w-[616px] w-full pt-20 pb-10 space-y-6">
-          <h1 className="main-heading text-white">
-            The only operating system for build & service contractors
-          </h1>
-          <p className="text-decemberSky text-lg font-medium font-jakarta">
-            All the power of big software, none of the pain. One platform—not
-            six—to manage jobs, crews, customers, and growth.
-          </p>
-          <div className="flex gap-2.5 items-center">
-            <button className="bg-red-linear h-10 primary-btn">
-              Create free account
-            </button>
-            <button className="flex gap-1.5 items-center font-myriad text-sm text-white cursor-pointer">
-              <CheckIcon />
-              No Credit Card Required
-            </button>
-          </div>
-        </div>
-      </section>
     </section>
   );
 };

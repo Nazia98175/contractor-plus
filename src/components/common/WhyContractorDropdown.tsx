2574d1d7ba9cace06dfbbd91b58fa8267f6a225b
suneel kumar <kumarsuneel162@gmail.com>
2025-05-07 23:41:18 +0530

    add header dropdown content

--- src/components/common/WhyContractorDropdown.tsx
+++ src/components/common/WhyContractorDropdown.tsx
@@ -1,31 +1,42 @@
 import Link from "next/link";
 import React from "react";
 import { ArrowIcon } from "./Icons";
-import { featureLinkslinks } from "./Helper";
+import { WhyContractorDropdownlinks } from "./Helper";
+import Image from "next/image";
 
 const WhyContractorDropdown = () => {
   return (
     <article className="flex flex-col justify-between p-2 gap-6 grow ">
-      <ul className="grid grid-cols-3 gap-x-6 gap-y-3 w-full overflow-auto">
-        {featureLinkslinks.map((link, index) => (
-          <li
-            className="group hover:bg-superSilver duration-300 ease-linear p-[6px]"
-            key={index}
-          >
-            <Link
-              href={link.href}
-              className="group group-hover:bg-lightBlack group-hover:!text-white"
+      <div className="flex items-start">
+        <ul className="grid  gap-x-6 gap-y-3 w-full overflow-auto">
+          {WhyContractorDropdownlinks.map((link, index) => (
+            <li
+              className="group hover:bg-superSilver duration-300 ease-linear p-[6px]"
+              key={index}
             >
-              <span className="header-li-dropdown group-hover:bg-lightBlack group-hover:!text-white">
-                {link.label}
-              </span>
-              <p className="text-sm font-inter text-lightBlack mt-2.5">
-                {link.description}
-              </p>
-            </Link>
-          </li>
-        ))}
-      </ul>
+              <Link
+                href={link.href}
+                className="group group-hover:bg-lightBlack group-hover:!text-white"
+              >
+                <span className="header-li-dropdown group-hover:bg-lightBlack group-hover:!text-white">
+                  {link.label}
+                </span>
+                <p className="text-sm font-inter text-lightBlack mt-2.5">
+                  {link.description}
+                </p>
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <Image
+          className="object-contain max-w-[420px] w-full"
+          src={"/images/webp/resources-dropdown-img.webp"}
+          alt="group-eng"
+          unoptimized
+          width={420}
+          height={290}
+        />
+      </div>
       <div className="flex items-center justify-between gap-6 p-[6px]">
         <Link
           className="flex items-center gap-2.5 text-xl font-medium text-lightBlack p-1"
@@ -34,22 +45,6 @@ const WhyContractorDropdown = () => {
           See All Features
           <ArrowIcon />
         </Link>
-        <div className="flex items-center gap-10">
-          <Link
-            className="flex items-center gap-2.5 text-xl font-medium text-lightBlack p-1"
-            href={"/"}
-          >
-            Integrations
-            <ArrowIcon />
-          </Link>
-          <Link
-            className="flex items-center gap-2.5 text-xl font-medium text-lightBlack p-1"
-            href={"/"}
-          >
-            Product Updates
-            <ArrowIcon />
-          </Link>
-        </div>
       </div>
     </article>
   );

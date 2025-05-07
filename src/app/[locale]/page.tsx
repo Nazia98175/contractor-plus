dcc964f4e4db5c13f25cc5b56c35b9e41393bf87
Tanu chaudhary <kaurtanu213@gmail.com>
2025-05-07 15:21:27 +0530

    add header

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,5 @@
 import Footer from "@/components/common/Footer";
+import Header from "@/components/common/Header";
 import LanguageSelector from "@/components/LanguageSelector";
 import { useTranslations } from "next-intl";
 
@@ -6,8 +7,10 @@ export default function Home() {
   const t = useTranslations();
 
   return (
-    <div className="flex flex-col gap-4 items-center justify-center min-h-screen">
+    <div className="flex flex-col gap-4  min-h-screen">
+      <Header />
       <h1 className="text-2xl font-bold">{t("heading")}</h1>
+
       <LanguageSelector />
       <Footer />
     </div>
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { LogoIcon } from "./Icons";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="bg-ruinedSmores w-full">
+      <div className="main-container py-5 flex  items-center ">
+        <LogoIcon />
+        <div className="flex gap-[22px]">
+          <Link href="/" className="header-li">
+            Why Contractor+?
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;

68b88f87f0d6b2ef3e263e98db24a808e0b418b1
rohit <rohit780790809@gmail.com>
2025-05-08 12:28:29 +0530

    background added

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -27,8 +27,8 @@ export default function Home() {
         <Finally />
         <CoreFeatures />
         <ContractorIndustry />
-        <OurReviews />
       </div>
+      <OurReviews />
       <Whatever />
       <MakeOperation />
       <EntireBusiness />
--- src/components/homepage/OurReviews.tsx
+++ src/components/homepage/OurReviews.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const OurReviews = () => {
-  return <div>OurReviews</div>;
+  return (
+    <section className="py-20">
+      <div className="flex justify-between items-center gap-3">
+        <h3 className="section-heading text-black">
+          Here you can find our reviews
+        </h3>
+        <div className="flex">
+          <img
+            className="max-w-[135px] w-full"
+            src="/images/svg/capterra-icon.svg"
+            alt=""
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default OurReviews;

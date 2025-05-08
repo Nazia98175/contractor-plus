0c84ade85b354f1b36e4c05bf8989711d7374082
Tanu chaudhary <kaurtanu213@gmail.com>
2025-05-08 15:44:53 +0530

    added section

--- src/components/common/Icons.tsx
+++ src/components/common/Icons.tsx
@@ -586,6 +586,33 @@ export const BigChiefAIIcon = () => (
     />
   </svg>
 );
+export const Pathbg = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1"
+    height="100%"
+    viewBox="0 0 1 382"
+    fill="none"
+  >
+    <path
+      d="M1 1C1 0.723858 0.776142 0.5 0.5 0.5C0.223858 0.5 1.20706e-08 0.723858 0 1L1 1ZM0.5 1L0 1L-1.66167e-05 381.145L0.499983 381.145L0.999983 381.145L1 1L0.5 1Z"
+      fill="url(#paint0_radial_22_8171)"
+    />
+    <defs>
+      <radialGradient
+        id="paint0_radial_22_8171"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(-1.28048e-05 20.4235) rotate(90) scale(354.479 37.0727)"
+      >
+        <stop stop-color="#1C2731" />
+        <stop offset="1" stop-color="#EE1E25" stop-opacity="0" />
+      </radialGradient>
+    </defs>
+  </svg>
+);
 export const InvoicingIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
--- src/components/homepage/ContractorIndustry.tsx
+++ src/components/homepage/ContractorIndustry.tsx
@@ -11,33 +11,7 @@ const ContractorIndustry = () => {
         for your contracting business
       </p>
       <div className="p-6">
-        <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="1"
-            height="382"
-            viewBox="0 0 1 382"
-            fill="none"
-          >
-            <path
-              d="M1 1C1 0.723858 0.776142 0.5 0.5 0.5C0.223858 0.5 1.20706e-08 0.723858 0 1L1 1ZM0.5 1L0 1L-1.66167e-05 381.145L0.499983 381.145L0.999983 381.145L1 1L0.5 1Z"
-              fill="url(#paint0_radial_22_8171)"
-            />
-            <defs>
-              <radialGradient
-                id="paint0_radial_22_8171"
-                cx="0"
-                cy="0"
-                r="1"
-                gradientUnits="userSpaceOnUse"
-                gradientTransform="translate(-1.28048e-05 20.4235) rotate(90) scale(354.479 37.0727)"
-              >
-                <stop stop-color="#1C2731" />
-                <stop offset="1" stop-color="#EE1E25" stop-opacity="0" />
-              </radialGradient>
-            </defs>
-          </svg>
-        </div>
+        <div className="flex gap-1.5 pt-0.5"></div>
       </div>
     </section>
   );

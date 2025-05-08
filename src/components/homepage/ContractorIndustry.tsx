441171b0ffa2a7ef1767fa1fa7044cd3271a319a
Tanu chaudhary <kaurtanu213@gmail.com>
2025-05-08 16:16:33 +0530

    add feature heading

--- src/components/homepage/ContractorIndustry.tsx
+++ src/components/homepage/ContractorIndustry.tsx
@@ -1,20 +1,7 @@
 import React from "react";
 
 const ContractorIndustry = () => {
-  return (
-    <section className="max-w-[991px] w-full mx-auto px-2">
-      <h3 className="sub-heading text-lightBlack font-semibold">
-        Core Features
-      </h3>
-      <p className="text-base font-jakarta font-medium mt-4 text-wallStreet">
-        Discover the powerful tools that make Contractor+ the ultimate solution
-        for your contracting business
-      </p>
-      <div className="p-6">
-        <div className="flex gap-1.5 pt-0.5"></div>
-      </div>
-    </section>
-  );
+  return <div></div>;
 };
 
 export default ContractorIndustry;
--- src/components/homepage/CoreFeatures.tsx
+++ src/components/homepage/CoreFeatures.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const CoreFeatures = () => {
-  return <div>CoreFeatures</div>;
+  return (
+    <section className="max-w-[991px] w-full mx-auto px-2">
+      <h3 className="sub-heading text-lightBlack font-semibold">
+        Core Features
+      </h3>
+      <p className="text-base font-jakarta font-medium mt-4 text-wallStreet">
+        Discover the powerful tools that make Contractor+ the ultimate solution
+        for your contracting business
+      </p>
+      <div className="p-6">
+        <div className="flex gap-1.5 pt-0.5"></div>
+      </div>
+    </section>
+  );
 };
 
 export default CoreFeatures;

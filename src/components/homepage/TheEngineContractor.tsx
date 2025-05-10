c183da349534f98036b359ae62d36730c78163c7
Tanu chaudhary <kaurtanu213@gmail.com>
2025-05-10 09:53:35 +0530

    working on translation

--- src/components/homepage/TheEngineContractor.tsx
+++ src/components/homepage/TheEngineContractor.tsx
@@ -1,16 +1,17 @@
+import { useTranslations } from "next-intl";
 import React from "react";
 
 const TheEngineContractor = () => {
+  const t = useTranslations("engine");
   return (
     <section className="xl:bg-[url('/images/webp/engine-bg.webp')] py-8 w-full bg-[100%_100%] overflow-hidden bg-contain bg-no-repeat max-w-[98%] min-[1440px]:max-w-[1364px] mx-auto  bg-none rounded-[22px] mt-8 md:mt-9">
       <div className="main-container flex md:justify-between flex-col items-center md:flex-row justify-center gap-8 md:py-8 md:!px-12">
         <div className="flex flex-col gap-1.5 md:max-w-[515px] w-full">
           <h2 className="section-heading text-white text-center md:text-left">
-            The engine 57,163 contractors run on
+            {t("heading")}
           </h2>
           <p className="text-base font-medium text-decemberSky text-center md:text-left">
-            All the power of big software, none of the pain. One platform—not
-            six—to manage jobs, crews, customers, and growth.
+            {t("desc")}
           </p>
         </div>
         <div className="flex flex-col max-w-[356px] w-full -space-y-2  items-center">

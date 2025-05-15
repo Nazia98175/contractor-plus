c6fbf2df5d07e40b0318fd2ec62ff77d98bbd089
suneel kumar <kumarsuneel162@gmail.com>
2025-05-15 10:42:57 +0530

    to check upadted code

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Footer from "@/components/common/Footer";
 import Header from "@/components/common/Header";
+import ParticlesComponent from "@/components/common/ParticlesComponent";
 import ContractorIndustry from "@/components/homepage/ContractorIndustry";
 import ContractorPlatforms from "@/components/homepage/ContractorPlatforms";
 import CoreFeatures from "@/components/homepage/CoreFeatures";
@@ -61,6 +62,7 @@ export default function Home() {
           fill
           className="absolute top-[50%] -z-50 left-0 object-cover"
         /> */}
+
         <ContractorPlatforms />
         <Finally />
       </div>

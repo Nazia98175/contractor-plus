b93e0d919b3a3e9615c0a86b5e24400f0bc05439
Tanu chaudhary <kaurtanu213@gmail.com>
2025-05-16 12:11:37 +0530

    correct hero video

--- src/components/homepage/Hero.tsx
+++ src/components/homepage/Hero.tsx
@@ -6,8 +6,8 @@ import VideoViewer from "./VideoViewer";
 const Hero = () => {
   const t = useTranslations("hero");
   return (
-    <section className="relative overflow-hidden max-w-[1920px] mx-auto">
-      <div className="flex items-end main-container z-20 relative pt-[292px]  pb-11 md:pb-20 lg:pt-[138px] lg:pb-36 xl:pb-[202px] ">
+    <section className="relative overflow-hidden">
+      <div className="flex items-end main-container z-20 relative pt-[292px] pb-11 md:pb-20 lg:pt-[138px] lg:pb-36 xl:pb-[202px] 3xl:min-h-[1000px] max-h-[1000px]">
         <div className="max-w-[616px] w-full sm:space-y-6">
           <TextAnimation animateOnScroll={false} delay={0.3}>
             <h1 className="main-heading text-white mb-1.5">{t("heading")}</h1>
@@ -28,7 +28,7 @@ const Hero = () => {
           </div>
         </div>
       </div>
-      <div className="absolute object-center lg:-right-[15%] w-full h-full max-h-[1200px] lg:max-h-[750px] top-0 aspect-video">
+      <div className="absolute object-bottom lg:-right-[15%] w-full h-full max-h-[1200px] lg:max-h-[750px] 3xl:max-h-[1024px] top-0 aspect-video">
         <VideoViewer />
       </div>
 

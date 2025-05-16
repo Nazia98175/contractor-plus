034e431ad178062b46e29973254f3c6023c9e8ce
Tanu chaudhary <kaurtanu213@gmail.com>
2025-05-16 23:29:55 +0530

    fix header hover

--- src/components/crmbussiness/TeamsUsingContractor.tsx
+++ src/components/crmbussiness/TeamsUsingContractor.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const TeamsUsingContractor = () => {
-  return <div>TeamsUsingContractor</div>;
+  return (
+    <div className="bg-white">
+      <p>
+        Teams using
+        <span className="linear-text">Contractor+ CRM never look back</span>
+      </p>
+    </div>
+  );
 };
 
 export default TeamsUsingContractor;
--- src/components/homepage/FeatureCard.tsx
+++ src/components/homepage/FeatureCard.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import React from "react";
-import TextAnimation from "../common/TextAnimation";
 
 interface FeatureCardProps {
   obj: {

eee4e35b1caf1707d95b431c2c2f449aace4dce1
rohit <rohit780790809@gmail.com>
2025-05-16 15:46:03 +0530

    blog post added

--- src/components/crmbussiness/Faq.tsx
+++ src/components/crmbussiness/Faq.tsx
@@ -1,10 +1,15 @@
 "use client";
 import React, { useState } from "react";
 import AnimateHeight from "react-animate-height";
+import FaqList from "./FaqList";
 
+type FaqItemType = {
+  question: string;
+  answer: string;
+};
 const Faq = () => {
-  const [FaqOpen, setOpen] = useState(0);
-  const handleopen = (index) => {
+  const [FaqOpen, setOpen] = useState<number | null>(0);
+  const handleToggle = (index: number) => {
     setOpen(FaqOpen === index ? null : index);
   };
   console.log(FaqOpen);
@@ -56,57 +61,13 @@ const Faq = () => {
         Frequently asked questions
       </p>
       <div className="max-w-[1190px] mx-auto px-2 pt-[27px]">
-        {faqitems.map((item, index) => (
-          <div
-            onClick={() => handleopen(index)}
+        {faqitems.map((item: FaqItemType, index: number) => (
+          <FaqList
             key={index}
-            className="mb-8 cursor-pointer"
-          >
-            <button className="flex justify-between items-center gap-5 cursor-pointer w-full">
-              <h3 className="paragraph-text text-white font-extrabold">
-                {item.question}
-              </h3>
-              <span>
-                {FaqOpen === index ? (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
-                      d="M5 12C5 11.4477 5.44772 11 6 11L18 11C18.5523 11 19 11.4477 19 12C19 12.5523 18.5523 13 18 13L6 13C5.44772 13 5 12.5523 5 12Z"
-                      fill="white"
-                    />
-                  </svg>
-                ) : (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                  >
-                    <path
-                      d="M12 19C11.7167 19 11.479 18.904 11.287 18.712C11.095 18.52 10.9993 18.2827 11 18V13H6C5.71667 13 5.479 12.904 5.287 12.712C5.095 12.52 4.99934 12.2827 5 12C5 11.7167 5.096 11.479 5.288 11.287C5.48 11.095 5.71734 10.9993 6 11H11V6C11 5.71667 11.096 5.479 11.288 5.287C11.48 5.095 11.7173 4.99934 12 5C12.2833 5 12.521 5.096 12.713 5.288C12.905 5.48 13.0007 5.71734 13 6V11H18C18.2833 11 18.521 11.096 18.713 11.288C18.905 11.48 19.0007 11.7173 19 12C19 12.2833 18.904 12.521 18.712 12.713C18.52 12.905 18.2827 13.0007 18 13H13V18C13 18.2833 12.904 18.521 12.712 18.713C12.52 18.905 12.2827 19.0007 12 19Z"
-                      fill="white"
-                    />
-                  </svg>
-                )}
-              </span>
-            </button>
-            <AnimateHeight
-              duration={500}
-              height={FaqOpen === index ? "auto" : 0}
-            >
-              <p className="text-[#D2D4D6] text-base font-jakarta max-w-[1113px] pt-4">
-                {item.answer}
-              </p>
-            </AnimateHeight>
-          </div>
+            data={item}
+            isOpen={FaqOpen === index}
+            onToggle={() => handleToggle(index)}
+          />
         ))}
       </div>
     </section>

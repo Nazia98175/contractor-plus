817b7d6d276a9517991db4c7625f5f0c3a8e96d0
Tanu chaudhary <kaurtanu213@gmail.com>
2025-05-19 16:51:18 +0530

    add pull

--- src/components/crmbussiness/FieldService.tsx
+++ src/components/crmbussiness/FieldService.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const FieldService = () => {
-  return <div>FieldService</div>;
+  return (
+    <div className="py-10 ">
+      <article className="field-service-card flex justify-between text-white py-20">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure esse
+        maxime distinctio commodi cupiditate, quo aliquam recusandae veritatis
+        possimus facilis?
+      </article>
+    </div>
+  );
 };
 
 export default FieldService;

18745b92ef1b01547cf779d510d647fbde45e421
rohit <rohit780790809@gmail.com>
2025-05-22 16:51:33 +0530

    loadder added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,29 @@
-import { ReactNode } from "react";
+"use client";
+import { ReactNode, useEffect, useState } from "react";
 import SmoothScroll from "@/components/common/SmoothScroll";
 type Props = {
   children: ReactNode;
 };
 
 export default function RootLayout({ children }: Props) {
+  const [Loading, setLoading] = useState(true);
+  console.log(Loading, "loading");
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
   return (
     <>
+      {Loading && (
+        <div className="h-screen w-full bg-red-400 text-green-400 z-[10000]">
+          <h2>loading...</h2>
+        </div>
+      )}
       <SmoothScroll />
       {children}
     </>

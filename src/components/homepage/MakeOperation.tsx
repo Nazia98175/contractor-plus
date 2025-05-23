8c1b92f3fc79be2804dbb38d753d5eef4e99dedb
rohit <rohit780790809@gmail.com>
2025-05-23 16:24:31 +0530

    make operation svg animate

--- src/components/homepage/MakeOperation.tsx
+++ src/components/homepage/MakeOperation.tsx
@@ -35,23 +35,79 @@ const MakeOperation: React.FC<TheWhateverProps> = ({ whateverOperation }) => {
   return (
     <section ref={ref} className="relative pt-16 z-10">
       <div className="hidden color-animation-1 lg:block absolute bottom-0 left-0 max-w-[40px] rotate-[-45deg] w-full h-[500px] rounded-[10px] bg-athenaBlue blur-[34px] opacity-20 pointer-events-none"></div>
-      <Image
+      {/* <Image
         className="object-cover -top-[10%] right-0 absolute z-0 pointer-events-none max-w-[700px] lg:block hidden color-animation"
         src="/images/webp/make-opration.webp"
         width={700}
         height={300}
         alt="gradient background"
         priority
-      />
+      /> */}
 
       <Image
-        className="object-cover -top-0 left-0 absolute z-0 pointer-events-none max-w-[700px] block lg:hidden color-animation-1"
+        className="object-cover -top-0 left-0 absolute z-0 pointer-events-none max-w-[700px] block lg:hidden "
         src="/images/webp/make-opration-mobile.webp"
         fill
         alt="gradient background"
         priority
       />
-
+      <span className="top-[0px] right-[-100px] absolute z-0 pointer-events-none max-w-[800px] w-full lg:block hidden">
+        <svg
+          className="svg-container"
+          xmlns="http://www.w3.org/2000/svg"
+          width="100%"
+          height="100%"
+          fill="none"
+          viewBox="0 0 1479 1112"
+        >
+          <g
+            filter="url(#filter0_f_783_6314)"
+            style={{ mixBlendMode: "plus-lighter" }}
+          >
+            <path
+              className="main-path"
+              stroke="url(#paint0_linear_783_6314)"
+              strokeLinecap="round"
+              strokeWidth="432"
+              d="m305 305 434.784 251.023 434.786 251.023"
+            ></path>
+          </g>
+          <defs>
+            <linearGradient
+              id="paint0_linear_783_6314"
+              x1="755.313"
+              x2="1065.72"
+              y1="874.455"
+              y2="654.336"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop offset="0.062" stopOpacity="0"></stop>
+              <stop offset="0.997" stopColor="#EE1E25"></stop>
+              <stop offset="1" stopColor="#fff"></stop>
+            </linearGradient>
+            <filter
+              id="filter0_f_783_6314"
+              width="1477.64"
+              height="1110.12"
+              x="0.964"
+              y="0.964"
+              colorInterpolationFilters="sRGB"
+              filterUnits="userSpaceOnUse"
+            >
+              <feFlood floodOpacity="0" result="BackgroundImageFix"></feFlood>
+              <feBlend
+                in="SourceGraphic"
+                in2="BackgroundImageFix"
+                result="shape"
+              ></feBlend>
+              <feGaussianBlur
+                result="effect1_foregroundBlur_783_6314"
+                stdDeviation="44"
+              ></feGaussianBlur>
+            </filter>
+          </defs>
+        </svg>
+      </span>
       <div className="main-container pb-10 relative">
         <TextAnimation animateOnScroll={true} delay={0.3}>
           <h3 className="section-heading font-semibold  gradient-text text-center">

2cc7a3928aad31f790f6ea69a88323d5c6bc4e97
Tanu chaudhary <kaurtanu213@gmail.com>
2025-05-28 11:44:02 +0530

    add prttier file

---
+++ src/components/hvca/WantingMore.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const WantingMore = () => {
+  return (
+    <div>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+        incidunt non tempore magni hic, eaque vel porro ipsam. Voluptatum odio,
+        eligendi ex fugiat quibusdam hic temporibus nulla inventore delectus
+        iste.
+      </h1>
+    </div>
+  );
+};
+
+export default WantingMore;

053a2faaf567a0e01a5158ce297058f349303293
Tanu chaudhary <kaurtanu213@gmail.com>
2025-05-28 13:10:56 +0530

    working on hvca page

--- src/components/hvca/WantingMore.tsx
+++ src/components/hvca/WantingMore.tsx
@@ -1,15 +1,33 @@
 import React from "react";
+import TextAnimation from "../common/TextAnimation";
+import { RedCurveLine } from "../common/Icons";
+
+const curvePositions = [0, 50, 100];
 
 const WantingMore = () => {
   return (
-    <div>
-      <h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
-        incidunt non tempore magni hic, eaque vel porro ipsam. Voluptatum odio,
-        eligendi ex fugiat quibusdam hic temporibus nulla inventore delectus
-        iste.
-      </h1>
-    </div>
+    <section>
+      <TextAnimation animateOnScroll={true} delay={0.3}>
+        <h2 className="section-heading gradient-text-2 z-30 mx-auto w-fit max-w-[1004px] text-center !font-black lg:!font-semibold">
+          Contractor+ is the only HVAC software that doesn’t leave you still
+          wanting more
+        </h2>
+      </TextAnimation>
+
+      <div className="relative mt-24 flex h-screen flex-col items-center justify-center bg-[url('/images/webp/blur-bg.webp')] bg-cover bg-no-repeat">
+        {curvePositions.map((topOffset, index) => (
+          <span
+            key={index}
+            className="absolute -left-[2%] z-30 block w-[108%]"
+            style={{ top: `${topOffset}px` }}
+          >
+            <RedCurveLine />
+          </span>
+        ))}
+
+        <div className="wanting-more-bg h-96 w-full max-w-[1232px]"></div>
+      </div>
+    </section>
   );
 };
 

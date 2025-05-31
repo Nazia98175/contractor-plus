75d93c6f08d2fa83b80928791091aea8ac71989b
Tanu chaudhary <kaurtanu213@gmail.com>
2025-05-31 23:33:46 +0530

    correct home page image

--- src/components/hvca/HvacHero.tsx
+++ src/components/hvca/HvacHero.tsx
@@ -122,7 +122,7 @@ const HvacHero = () => {
           className="pointer-events-none absolute top-0 left-0 z-20 block h-full w-full object-cover lg:hidden"
           layout="lazy"
         />
-        <div className="main-container relative z-20 flex items-end pt-[269px] pb-9 md:pb-[100px] lg:pt-[180px] lg:pb-[150px] xl:pb-[208px]">
+        <div className="main-container relative z-20 flex items-end pt-[395px] pb-9 sm:pt-[269px] md:pb-[100px] lg:pt-[180px] lg:pb-[150px] xl:pb-[208px]">
           <HvacGlowHeroMobileIcon />
           <HvacGlowHeroDesktopIcon />
           <div className="relative z-30 w-full sm:space-y-6 lg:max-w-[750px]">
@@ -151,11 +151,11 @@ const HvacHero = () => {
         <div className="absolute right-[2%] bottom-0 z-[1000] hidden h-[70%] w-[314px] lg:flex xl:right-[7%]">
           <HvacHeroSlider features={features} />
         </div>
-        <div className="absolute top-0 aspect-video h-full max-h-[1200px] w-full object-bottom lg:right-[-15%] lg:max-h-[750px]">
-          <VideoOptimizer
+        <div className="absolute top-0 right-0 h-full max-h-[1200px] w-full max-w-[945px] lg:max-h-[750px]">
+          {/* <VideoOptimizer
             highResUrl="/video/hero-video.mp4"
             lowResUrl="/video/hero-video.mp4"
-          />
+          /> */}
           {/* <video
             autoPlay
             muted
@@ -165,11 +165,25 @@ const HvacHero = () => {
             src="/video/hero-video.mp4"
           ></video> */}
           <Image
+            alt=""
+            src={"/images/webp/hvac-hero.webp"}
+            width={945}
+            height={729}
+            className="hidden w-full object-right sm:block"
+          />
+          <Image
+            alt=""
+            src={"/images/webp/hvac-hero-mobile.webp"}
+            width={945}
+            height={729}
+            className="block w-full object-contain sm:hidden"
+          />
+          <Image
             priority
             fill
             unoptimized
             className="absolute -top-[6%] !left-[-1%] hidden !h-[111%] w-full object-cover lg:block"
-            src={"/images/webp/hero-video-ovelay.webp"}
+            src="/images/webp/hero-video-ovelay.webp"
             alt="hero-video-ovelay"
           />
           {/* <div className="bg-kuroiBlack absolute bottom-[-3px] z-[1000] hidden h-[10px] w-full lg:block"></div> */}

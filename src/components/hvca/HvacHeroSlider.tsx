4a768bfcfcbe0713ac0a5d4473dcdd94f61c42e8
Tanu chaudhary <kaurtanu213@gmail.com>
2025-06-01 09:52:31 +0530

    correct slider img and bg

--- src/components/hvca/HvacHeroSlider.tsx
+++ src/components/hvca/HvacHeroSlider.tsx
@@ -33,7 +33,7 @@ const HvacHeroSlider: React.FC<HvacHeroSliderProps> = ({ features }) => {
     >
       {features.map((feature, index) => (
         <SwiperSlide key={index}>
-          <div className="border-redPigment hero-slider hero-slide-content overflow-hidden rounded-3xl border p-4 backdrop-blur-[26px] transition-opacity duration-500 xl:p-6">
+          <div className="border-redPigment/35 hero-slider hero-slide-content overflow-hidden rounded-3xl border p-4 backdrop-blur-[26px] transition-opacity duration-500 xl:p-6">
             <Image
               className="hero-img active-img pointer-events-none absolute right-0 -z-40"
               src="/images/svg/hero-slider-animated-svg.svg"

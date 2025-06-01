26207ccd31234ffc05ccdd98aee1d19a0b1f5515
suneel kumar <kumarsuneel162@gmail.com>
2025-06-01 12:02:57 +0530

    fix bg

--- src/app/[locale]/hvac/page.tsx
+++ src/app/[locale]/hvac/page.tsx
@@ -26,8 +26,10 @@ const page = () => {
         <WantingMore />
         <EraOfSoftware />
       </div>
-      <AwardBadges />
-      <HvacReview />
+      <div className="relative overflow-hidden">
+        <AwardBadges />
+        <HvacReview />
+      </div>
       <HvacSoftwareService />
       <TrustBarHvca platforms={platforms} />
       {/* <Whatever

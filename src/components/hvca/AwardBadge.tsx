53c00ed7e0e054fd0231e103f0a8495ffc9d319a
rohit <rohit780790809@gmail.com>
2025-06-11 14:28:24 +0530

    section fix

--- src/components/hvca/AwardBadge.tsx
+++ src/components/hvca/AwardBadge.tsx
@@ -22,11 +22,7 @@ export default function AwardBadges() {
             <ArrowIcon fill="#fff" />
           </span>
         </button>
-<<<<<<< HEAD
-        <CardRequiredButton className="text-winterWay" />
-=======
         {/* <CardRequiredButton className="text-winterWay" /> */}
->>>>>>> 0b5dedb2bbbe34bd7223bd3d72e8923a029cf3a0
       </div>
       <AwardsTagsImg />
     </section>

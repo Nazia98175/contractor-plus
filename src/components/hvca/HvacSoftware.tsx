959c6d87a0337d68afc82b5d7a19e688e43ae37d
Tanu chaudhary <kaurtanu213@gmail.com>
2025-06-11 11:06:11 +0530

    remove font bold from css

--- src/components/hvca/HvacSoftware.tsx
+++ src/components/hvca/HvacSoftware.tsx
@@ -15,7 +15,7 @@ const HvacSoftware = () => {
         {softwareCardData.map((card, index) => (
           <article
             key={index}
-            className="software-bg card-shine relative flex w-full max-w-[390px] cursor-pointer flex-col items-center justify-center gap-2.5 overflow-hidden rounded-lg p-2.5"
+            className="software-bg card-shine relative flex w-full max-w-[390px] cursor-pointer flex-col items-center gap-2.5 overflow-hidden rounded-lg p-2.5"
           >
             <AlertIcon />
             <h4 className="text-winterWay relative mx-auto w-full text-center text-base leading-[130%] font-medium text-ellipsis text-shadow-[0px_0px_20px_rgba(255,255,255,0.50)] sm:text-lg xl:text-[22px]">

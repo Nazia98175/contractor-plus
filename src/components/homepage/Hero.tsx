38b8b83f431e1fb5dd8d7fed633b7892f315cf9b
Tanu chaudhary <kaurtanu213@gmail.com>
2025-06-12 17:55:11 +0530

    add pull

--- src/components/homepage/Hero.tsx
+++ src/components/homepage/Hero.tsx
@@ -1,14 +1,11 @@
 "use client";
 import dynamic from "next/dynamic";
 import Image from "next/image";
-import {
-  CheckIcon,
-  HeroAnimatedIcon,
-  HeroAnimatedMobileIcon,
-} from "../common/Icons";
-import TextAnimation from "../common/TextAnimation";
-import FreeAccountButton from "../common/FreeAccountButton";
 import CardRequiredButton from "../common/CardRequiredButton";
+import CardReveal from "../common/CardReveal";
+import FreeAccountButton from "../common/FreeAccountButton";
+import { HeroAnimatedIcon, HeroAnimatedMobileIcon } from "../common/Icons";
+import TextAnimation from "../common/TextAnimation";
 const VideoOptimizer = dynamic(() => import("./VideoOptimizer"), {
   ssr: false,
 });
@@ -28,8 +25,8 @@ const Hero = ({ homePageContent }: { homePageContent: any }) => {
   } = homePageContent ?? {};
   return (
     <section className="lg:bg-kuroiBlack hero-mobile-bg relative z-20 overflow-hidden">
-      <div className="bg-kuroiBlack pointer-events-none absolute -bottom-[5%] left-1/2 z-20 h-24 w-[120%] -translate-x-1/2 blur-[13px]"></div>
-      <div className="bg-kuroiBlack pointer-events-none absolute -bottom-[5%] left-1/2 z-20 h-16 w-[120%] -translate-x-1/2 blur-[8px]"></div>
+      <div className="bg-kuroiBlack pointer-events-none absolute -bottom-[5%] left-1/2 z-20 hidden h-24 w-[120%] -translate-x-1/2 blur-[13px] lg:block"></div>
+      <div className="bg-kuroiBlack pointer-events-none absolute -bottom-[5%] left-1/2 z-20 hidden h-16 w-[120%] -translate-x-1/2 blur-[8px] lg:block"></div>
       <div className="bg-athenaBlue absolute top-56 right-0 h-6 w-full max-w-[800px] rotate-45 blur-[40px]"></div>
       <HeroAnimatedIcon />
       <HeroAnimatedMobileIcon />
@@ -41,24 +38,31 @@ const Hero = ({ homePageContent }: { homePageContent: any }) => {
         className="pointer-events-none absolute top-0 left-0 z-20 block h-full w-full object-cover lg:hidden"
         layout="lazy"
       />
-      <div className="main-container relative z-20 flex items-end pt-[269px] pb-9 md:pb-[100px] lg:pt-[140px] lg:pb-[150px] xl:pb-[196px]">
+      <div className="main-container relative z-10 flex items-end pt-[269px] pb-9 md:pb-[100px] lg:pt-[140px] lg:pb-[150px] xl:pb-[196px]">
         <div className="relative z-30 w-full sm:space-y-6 lg:max-w-[628px]">
-          <TextAnimation animateOnScroll={false} delay={3}>
+          <TextAnimation animateOnScroll={false} delay={0.6}>
             <h1 className="main-heading gradient-text mb-4">{hero_title}</h1>
           </TextAnimation>
-          <TextAnimation animateOnScroll={false} delay={3}>
+          <TextAnimation animateOnScroll={false} delay={0.6}>
             <p className="text-decemberSky mb-4 text-xs font-semibold sm:text-sm md:text-base md:font-medium lg:text-lg">
               {hero_description}
             </p>
           </TextAnimation>
 
-          <div className="flex w-full flex-col items-center gap-2.5 sm:w-fit">
+          <CardReveal
+            staggerDelay={0.6}
+            animationDuration={0.6}
+            distance={50}
+            delay={0.3}
+            animateOnScroll={false}
+            className="flex w-full flex-col items-center gap-2.5 sm:w-fit"
+          >
             <FreeAccountButton showIcon={false} text={cta_button_text} />
             <CardRequiredButton text={ncc_text} />
-          </div>
+          </CardReveal>
         </div>
       </div>
-      <div className="absolute top-0 aspect-video h-full max-h-[1200px] w-full object-bottom lg:right-[-15%] lg:max-h-[750px]">
+      <div className="absolute top-0 -z-10 aspect-video h-full max-h-[1200px] w-full object-bottom lg:right-[-15%] lg:max-h-[750px]">
         <VideoOptimizer
           highResUrl={"/video/hero-video.mp4"}
           lowResUrl="/video/hero-video.mp4"

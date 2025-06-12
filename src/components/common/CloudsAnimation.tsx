d13dc8e4f3420fcc37f231d169201cf2cf7efa10
rohit <rohit780790809@gmail.com>
2025-06-12 16:32:39 +0530

    cloud animation fix

--- src/components/common/CloudsAnimation.tsx
+++ src/components/common/CloudsAnimation.tsx
@@ -16,19 +16,19 @@ const CloudsAnimation = ({
 }) => {
   return (
     <div
-      className={`${className} clouds absolute right-0 left-0 z-20 h-full overflow-hidden`}
+      className={`${className} clouds absolute right-0 -bottom-[9%] left-0 z-20 h-full overflow-hidden`}
     >
       <div
-        className={`${cloud1Class} clouds-1 absolute right-0 bottom-[62px] left-0 h-20 w-full bg-[url("/images/png/pngwing.png")] bg-contain bg-repeat-x opacity-30 sm:bottom-[50px] sm:h-16 md:bottom-[30px] md:h-20 lg:bottom-0`}
+        className={`${cloud1Class} clouds-1 absolute right-0 bottom-[62px] left-0 h-12 w-full bg-[url("/images/png/pngwing.png")] bg-contain bg-repeat-x opacity-30 grayscale-75 sm:bottom-[50px] sm:h-16 md:bottom-[30px] md:h-20 lg:bottom-0`}
       ></div>
       <div
-        className={`${cloud2Class} clouds-2 absolute right-0 bottom-[51px] left-0 h-16 w-full bg-[url("/images/png/pngwing-5.png")] bg-contain bg-repeat-x opacity-30 sm:bottom-[60px] md:bottom-[50px] md:h-20 lg:bottom-0 lg:h-[140px]`}
+        className={`${cloud2Class} clouds-2 absolute right-0 bottom-[51px] left-0 h-16 w-full bg-[url("/images/png/pngwing-5.png")] bg-contain bg-repeat-x opacity-30 grayscale-75 sm:bottom-[60px] md:bottom-[50px] md:h-20 lg:bottom-0 lg:h-[140px]`}
       ></div>
       <div
-        className={`${cloud3Class} clouds-2 absolute right-0 bottom-0 left-0 h-16 w-full bg-[url("/images/png/pngwing-2.png")] bg-contain bg-repeat-x opacity-30 md:h-20 lg:flex lg:h-[140px]`}
+        className={`${cloud3Class} clouds-2 absolute right-0 bottom-0 left-0 h-16 w-full bg-[url("/images/png/pngwing-2.png")] bg-contain bg-repeat-x opacity-30 grayscale-75 md:h-20 lg:h-[140px]`}
       ></div>
       <div
-        className={`${cloud4Class} clouds-3 absolute right-0 bottom-0 left-0 w-full bg-[url("/images/png/pngwing-3.png")] bg-contain bg-repeat-x opacity-30 sm:block sm:h-20 md:h-28 lg:h-[160px]`}
+        className={`${cloud4Class} clouds-3 absolute right-0 bottom-0 left-0 w-full bg-[url("/images/png/pngwing-3.png")] bg-contain bg-repeat-x opacity-30 grayscale-75 sm:h-20 md:h-28 lg:h-[160px]`}
       ></div>
       <img
         className={`${imageClass} absolute bottom-4 left-0 h-28 w-full object-left-bottom`}

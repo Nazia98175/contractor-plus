6c4546e5a14f7995689e30b1cac70058be4cc03a
rakesh olla <rakesh10448olla@gmail.com>
2025-06-13 16:49:42 +0530

    industry section added

--- src/app/[locale]/why-contractor/page.tsx
+++ src/app/[locale]/why-contractor/page.tsx
@@ -1,6 +1,7 @@
 import BuildBusinessOnBlood from "@/components/why-contractor/BuildBusinessOnBlood";
 import CantScale from "@/components/why-contractor/CantScale";
 import FastestWayToLoose from "@/components/why-contractor/FastestWayToLoose";
+import IndustryShifted from "@/components/why-contractor/IndustryShifted";
 import NewWayToWin from "@/components/why-contractor/NewWayToWin";
 import WhyContractorHero from "@/components/why-contractor/WhyContractorHero";
 import React from "react";
@@ -9,6 +10,7 @@ const WhyContractorPage = () => {
   return (
     <main className="bg-kuroiBlack -z-[2]">
       <WhyContractorHero />
+      <IndustryShifted />
       <CantScale />
       <BuildBusinessOnBlood />
       <FastestWayToLoose />

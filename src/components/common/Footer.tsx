6dcd385f165642ad63d2a912466832a5a88b5441
rohit <rohit780790809@gmail.com>
2025-06-19 17:03:34 +0530

    footer fix

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -78,7 +78,6 @@ const Footer: React.FC<TheFooterProps> = ({ footer }) => {
       <span className="pointer-events-none absolute -top-[200px] left-0 hidden max-w-[300px] lg:block xl:top-[-314px] xl:max-w-[550px]">
         <FooterAnimatedIcon />
       </span>
-      <div className="button-0 absolute left-0 h-[500px] w-[100px] rotate-[45deg] bg-red-300 opacity-30 blur-3xl"></div>
       <BlueIcon className="pointer-events-none absolute top-[-364px] right-0 hidden h-full max-h-[1002px] w-full max-w-[463px] lg:block" />
       <div className="main-container">
         <div className="mx-auto flex max-w-[169px] flex-col items-center justify-center space-y-5 pb-8 text-center sm:max-w-[222px] xl:pb-12">

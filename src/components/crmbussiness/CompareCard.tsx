106c501c5711982481013fbf646da87fe12ebf43
rohit <rohit780790809@gmail.com>
2025-06-19 16:01:21 +0530

    fix kindadorrable section

--- src/components/crmbussiness/CompareCard.tsx
+++ src/components/crmbussiness/CompareCard.tsx
@@ -4,16 +4,19 @@ import { CheckIcon, CloseIcon } from "../common/Icons";
 
 interface CompareCardProps {
   feature: CompareFeature;
+  slug?: string;
 }
 
-const CompareCard: React.FC<CompareCardProps> = ({ feature }) => {
+const CompareCard: React.FC<CompareCardProps> = ({ feature, slug }) => {
   return (
-    <article className="flex flex-col p-2 space-y-2 border-b border-superSilver">
-      <h5 className="text-center text-winterWay tracking-[0.1px] font-myriad leading-[19px]">
+    <article
+      className={`flex flex-col space-y-2 border-b p-2 ${slug === "estimate" ? "border-superSilver bg-[rgba(255,255,255,0.80)]" : "border-superSilver bg-transparent"}`}
+    >
+      <h5 className="text-winterWay font-myriad text-center leading-[19px] tracking-[0.1px]">
         {feature.featureName}
       </h5>
-      <div className="p-1 flex w-full">
-        <button className="w-1/2 flex justify-center items-center border-r border-decemberSky">
+      <div className="flex w-full p-1">
+        <button className="border-decemberSky flex w-1/2 items-center justify-center border-r">
           {feature.ourProduct == "available" ? (
             <CheckIcon width={24} height={24} className="max-w-6 min-w-6" />
           ) : (
@@ -22,7 +25,7 @@ const CompareCard: React.FC<CompareCardProps> = ({ feature }) => {
             </span>
           )}
         </button>
-        <button className="w-1/2 flex justify-center items-center">
+        <button className="flex w-1/2 items-center justify-center">
           {feature.competitorsNote !== null ? (
             // <CheckIcon width={24} height={24} className="max-w-6 min-w-6" />
             <p>{feature?.competitorsNote}</p>

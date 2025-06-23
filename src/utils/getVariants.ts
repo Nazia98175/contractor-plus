a1bd3506f718391479e39171911b0b675d40e6f8
Tanu chaudhary <kaurtanu213@gmail.com>
2025-06-23 12:24:38 +0530

    add some styles in primary variant

--- src/utils/getVariants.ts
+++ src/utils/getVariants.ts
@@ -7,7 +7,7 @@ export const themeClassMap: Record<string, string> = {
 
 export const variantsForm = {
   default: {
-    title: "text-decemberSky sub-heading font-extrabold",
+    title: "text-white sub-heading font-extrabold",
     subtitle: "text-secondary",
     input: "bg-white text-gray-900 placeholder:text-secondary",
   },

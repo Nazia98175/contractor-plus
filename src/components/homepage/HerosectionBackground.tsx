077c1c79ceff2acfc51f461d95779d09d1b5c8a9
suneel kumar <kumarsuneel162@gmail.com>
2025-06-23 12:35:47 +0530

    fix animation

--- src/components/homepage/HerosectionBackground.tsx
+++ src/components/homepage/HerosectionBackground.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const HerosectionBackground = () => {
   return (
     <>
-      <div className="bg-athenaBlue absolute top-56 right-0 h-6 w-full max-w-[800px] rotate-45 blur-[40px] lg:hidden"></div>
+      {/* <div className="bg-athenaBlue absolute top-56 right-0 h-6 w-full max-w-[800px] rotate-45 blur-[40px] lg:hidden"></div> */}
       <img
         className="absolute top-0 left-0 hidden max-h-[800px] w-full max-w-[800px] object-center sm:block"
         src={"/images/webp/hero-red-line.webp"}

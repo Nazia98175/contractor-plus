e6a0c30da08ba0cd7ca5024613ee33fd191c74f6
Tanu chaudhary <kaurtanu213@gmail.com>
2025-06-27 13:27:34 +0530

    working on custom select

--- src/components/blog/BlogHero.tsx
+++ src/components/blog/BlogHero.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useRef } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { SearchIcon } from "lucide-react";
+import CustomSelect from "./CustomSelect";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -25,17 +26,17 @@ const BlogHero = () => {
   }, []);
 
   return (
-    <div className="relative h-screen overflow-hidden">
-      <div className="relative z-10 pt-52 pr-3 text-center text-4xl font-extrabold sm:pr-6 sm:text-5xl lg:pr-10 lg:text-6xl xl:text-[72px]">
+    <div className="relative h-screen overflow-hidden xl:h-[90vh]">
+      <div className="relative z-10 pt-44 pr-3 text-center text-4xl font-extrabold sm:pr-6 sm:text-5xl lg:pr-10 lg:text-6xl xl:text-[72px]">
         <h1 className="gradient-text-shadow absolute bottom-0 left-1/2 z-0 -translate-x-1/2 blur-[46px]">
           Contractor+ HQ
         </h1>
         <h1 className="gradient-text-shadow relative z-10">Contractor+ HQ</h1>
       </div>
-      <div className="relative z-30 mx-auto mt-16 flex w-full max-w-[788px] items-center justify-center rounded-lg bg-[rgba(12,13,17,0.42)] p-2.5 backdrop-blur-[42px]">
-        <select name="" id=""></select>
-        <div className="border-secondary flex h-10 w-full items-center rounded-md border pl-3.5">
-          <SearchIcon color="white" />
+      <div className="relative z-30 mx-auto mt-16 flex w-full max-w-[788px] items-center justify-center gap-2 rounded-lg bg-[rgba(12,13,17,0.42)] p-2.5 backdrop-blur-[42px]">
+        <CustomSelect />
+        <div className="border-secondary flex h-10 w-full items-center rounded-lg border pl-3.5">
+          <SearchIcon color="#ADB1B5" />
           <input
             type="text"
             autoFocus
@@ -45,7 +46,6 @@ const BlogHero = () => {
         </div>
       </div>
 
-      {/* IMAGE ON TOP (foreground parallax) */}
       <div
         ref={bgRef}
         className="absolute inset-0 z-20 bg-cover bg-center"
--- src/components/blog/CustomSelect.tsx
+++ src/components/blog/CustomSelect.tsx
@@ -1,7 +1,22 @@
+import { ChevronDown, ConstructionIcon } from "lucide-react";
 import React from "react";
 
 const CustomSelect = () => {
-  return <div>CustomSelect</div>;
+  return (
+    <div className="w-full max-w-[294px]">
+      <button className="border-cyanBlue flex h-10 w-full max-w-[294px] items-center rounded-md border">
+        <ConstructionIcon color="white" className="mr-2 ml-3.5" />
+        <span className="text-decemberSky w-full text-start tracking-[0.1px]">
+          General Contractor
+        </span>
+        <span className="border-wallStreet flex h-full items-center justify-center border-l">
+          <ChevronDown color="#D2D4D6" className="mx-2" />
+        </span>
+      </button>
+
+      <div className="absolute z-50 w-full"></div>
+    </div>
+  );
 };
 
 export default CustomSelect;

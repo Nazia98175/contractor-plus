f4455f20d330de970424d4116bbd2204a1d8834c
Tanu chaudhary <kaurtanu213@gmail.com>
2025-06-28 14:32:14 +0530

    testing blog page

--- src/components/blog/LatestFromContractor.tsx
+++ src/components/blog/LatestFromContractor.tsx
@@ -15,7 +15,7 @@ const LatestFromContractor = () => {
       title: "UX review presentations",
       description:
         "How do you create compelling presentations that wow your colleagues and impress your managers?",
-      image: "/images/webp/blog-card-3.webp",
+      image: "/images/webp/latest-blog-1.webp",
       link: "/articles/large",
       tags: ["Leadership", "Management"],
       second: [
@@ -26,7 +26,7 @@ const LatestFromContractor = () => {
           title: "Migrating to Linear 101",
           description:
             "Linear helps streamline software projects, sprints, tasks, and bug tracking. Here’s how to get...",
-          image: "/images/webp/blog-card-5.webp",
+          image: "/images/webp/latest-blog-2.webp",
           link: "/articles/small-1",
           tags: ["Architecture", "Planning"],
         },
@@ -37,7 +37,7 @@ const LatestFromContractor = () => {
           title: "Building your API Stack",
           description:
             "The rise of RESTful APIs has been met by a rise in tools for creating, testing, and manag...",
-          image: "/images/webp/blog-card-2.webp",
+          image: "/images/webp/latest-blog-3.webp",
           link: "/articles/small-2",
           tags: ["Design", "Frameworks"],
         },
@@ -61,7 +61,7 @@ const LatestFromContractor = () => {
           title: "Building your API Stack",
           description:
             "Linear helps streamline software projects, sprints, tasks, and bug tracking. Here’s how to get",
-          image: "/images/webp/blog-card-5.webp",
+          image: "/images/webp/latest-blog-2.webp",
           link: "/articles/small-3",
           tags: ["Architecture", "Planning"],
         },
@@ -72,7 +72,77 @@ const LatestFromContractor = () => {
           title: "Building your API Stack",
           description:
             "The rise of RESTful APIs has been met by a rise in tools for creating, testing, and manag",
-          image: "/images/webp/blog-card-2.webp",
+          image: "/images/webp/latest-blog-3.webp",
+          link: "/articles/small-4",
+          tags: ["Design", "Frameworks"],
+        },
+      ],
+    },
+    {
+      id: 7,
+      category: "Contractor",
+      date: "1 Jan 2023",
+      title: "UX review presentations 2",
+      description:
+        "How do you create compelling presentations that wow your colleagues and impress your managers?",
+      image: "/images/webp/blog-card-3.webp",
+      link: "/articles/large-2",
+      tags: ["Leadership", "Management"],
+      second: [
+        {
+          id: 8,
+          category: "Contractor",
+          date: "1 Jan 2023",
+          title: "Building your API Stack",
+          description:
+            "Linear helps streamline software projects, sprints, tasks, and bug tracking. Here’s how to get",
+          image: "/images/webp/latest-blog-2.webp",
+          link: "/articles/small-3",
+          tags: ["Architecture", "Planning"],
+        },
+        {
+          id: 9,
+          category: "Contractor",
+          date: "1 Jan 2023",
+          title: "Building your API Stack",
+          description:
+            "The rise of RESTful APIs has been met by a rise in tools for creating, testing, and manag",
+          image: "/images/webp/latest-blog-3.webp",
+          link: "/articles/small-4",
+          tags: ["Design", "Frameworks"],
+        },
+      ],
+    },
+    {
+      id: 10,
+      category: "Contractor",
+      date: "1 Jan 2023",
+      title: "UX review presentations 2",
+      description:
+        "How do you create compelling presentations that wow your colleagues and impress your managers?",
+      image: "/images/webp/blog-card-3.webp",
+      link: "/articles/large-2",
+      tags: ["Leadership", "Management"],
+      second: [
+        {
+          id: 11,
+          category: "Contractor",
+          date: "1 Jan 2023",
+          title: "Building your API Stack",
+          description:
+            "Linear helps streamline software projects, sprints, tasks, and bug tracking. Here’s how to get",
+          image: "/images/webp/latest-blog-2.webp",
+          link: "/articles/small-3",
+          tags: ["Architecture", "Planning"],
+        },
+        {
+          id: 12,
+          category: "Contractor",
+          date: "1 Jan 2023",
+          title: "Building your API Stack",
+          description:
+            "The rise of RESTful APIs has been met by a rise in tools for creating, testing, and manag",
+          image: "/images/webp/latest-blog-3.webp",
           link: "/articles/small-4",
           tags: ["Design", "Frameworks"],
         },
@@ -97,7 +167,7 @@ const LatestFromContractor = () => {
 
   return (
     <section className="custom-pagination relative z-20 mx-auto w-full max-w-[1224px] px-2 pt-8">
-      <h2 className="text-eerieBlack pb-4 text-2xl font-semibold sm:pb-7">
+      <h2 className="text-eerieBlack pb-4 text-2xl font-semibold sm:pb-6">
         Latest from Contractor+ HQ
       </h2>
       <Swiper
@@ -149,7 +219,7 @@ const LatestFromContractor = () => {
                 <div className="hidden lg:block">
                   <BlogCard article={group} variant="large" />
                 </div>
-                <div className="flex w-full flex-col justify-between gap-6 sm:flex-row lg:w-fit lg:flex-col">
+                <div className="flex h-full w-full flex-col gap-6 sm:flex-row lg:w-fit lg:flex-col">
                   {group.second.map((article) => (
                     <BlogCard
                       key={article.id}
@@ -162,13 +232,13 @@ const LatestFromContractor = () => {
             ))}
       </Swiper>
 
-      <div className="relative mt-6 flex w-full items-center justify-between gap-3 lg:mt-10 xl:mt-12">
-        <div className="swiper-button-prev3 text-flintstone hidden cursor-pointer items-center gap-2 text-sm font-medium md:flex">
-          <ArrowLeft color="#667085" /> Previous
+      <div className="relative mt-6 flex w-full items-center justify-between gap-3 border-t border-[#eaecf0] pt-5">
+        <div className="swiper-button-prev3 text-flintstone flex cursor-pointer items-center gap-2 text-sm font-medium">
+          <ArrowLeft height={20} width={20} color="#667085" /> Previous
         </div>
         <div className="swiper-pagination-real-time-3 flex items-center justify-center gap-1" />
-        <div className="swiper-button-next3 text-flintstone hidden cursor-pointer items-center gap-2 text-sm font-medium md:flex">
-          Next <ArrowRight color="#667085" />
+        <div className="swiper-button-next3 text-flintstone flex cursor-pointer items-center gap-2 text-sm font-medium">
+          Next <ArrowRight height={20} width={20} color="#667085" />
         </div>
       </div>
     </section>
--- src/utils/getVariants.ts
+++ src/utils/getVariants.ts
@@ -86,10 +86,10 @@ export const tagColors: Record<string, { bg: string; text: string }> = {
 export const variantsBlogCard = {
   small: {
     wrapper:
-      "group flex flex-col gap-6 lg:flex-row transition-all duration-200 ease-in",
+      "group flex flex-col gap-6 h-full lg:flex-row transition-all duration-200 ease-in",
     imageWrapper:
-      "overflow-hidden rounded-lg max-h-[240px] lg:max-h-[200px] min-h-[200px] h-full",
-    contentWrapper: "h-auto w-full ",
+      "overflow-hidden rounded-lg lg:w-1/2 max-h-[240px] lg:max-h-[200px] min-h-[150px] h-full",
+    contentWrapper: "h-auto w-full lg:w-[49%] ",
     title: "text-lg text-eerieBlack font-semibold",
   },
   large: {

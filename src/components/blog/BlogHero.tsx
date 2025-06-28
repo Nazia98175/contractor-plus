cd15ccf6958b52933dd9fbad9665b87fcf8be088
Tanu chaudhary <kaurtanu213@gmail.com>
2025-06-28 11:07:43 +0530

    add search button in blog hero

--- src/components/blog/BlogHero.tsx
+++ src/components/blog/BlogHero.tsx
@@ -1,10 +1,11 @@
 "use client";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { SearchIcon } from "lucide-react";
+
 import { useEffect, useRef, useState } from "react";
 import CustomSelect from "./CustomSelect";
 import { contractorTypes } from "../common/Helper";
+import { SearchIcon } from "../common/Icons";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -30,7 +31,7 @@ const BlogHero = () => {
   return (
     <div className="relative h-[90vh]">
       <div className="relative z-10 pt-44 pr-3 text-center text-4xl font-extrabold sm:pr-6 sm:text-5xl lg:pr-10 lg:text-6xl xl:text-[72px]">
-        <h1 className="gradient-text-shadow absolute bottom-0 left-1/2 z-0 -translate-x-1/2 blur-[46px]">
+        <h1 className="gradient-text-shadow absolute bottom-0 left-1/2 z-0 -translate-x-1/2 blur-[26px]">
           Contractor+ HQ
         </h1>
         <h1 className="gradient-text-shadow relative z-10">Contractor+ HQ</h1>
@@ -42,14 +43,19 @@ const BlogHero = () => {
           onChange={(option) => setSelectedValue(option?.value || "")}
           className="sm:max-w-[294px]"
         />
-        <div className="border-secondary flex h-10 w-full items-center rounded-lg border pl-3.5">
-          <SearchIcon color="#ADB1B5" />
-          <input
-            type="text"
-            autoFocus
-            placeholder="Most popular articles"
-            className="w-full px-3 text-white placeholder:text-white focus:outline-none"
-          />
+        <div className="flex w-full items-center gap-2.5">
+          <div className="border-secondary flex h-10 w-full items-center rounded-lg border pl-3.5">
+            <SearchIcon />
+            <input
+              type="text"
+              autoFocus
+              placeholder="Select Contractor + HQ"
+              className="text-decemberSky placeholder:text-decemberSky w-full px-3 tracking-[0.1px] focus:outline-none"
+            />
+          </div>
+          <button className="bg-romanRed h-10 w-10 rounded-lg p-2 duration-300 hover:opacity-80">
+            <SearchIcon color="#fff" />
+          </button>
         </div>
       </div>
 

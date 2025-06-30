0ebbe67609d50f3f6d52173b390e468bc5d75f1a
Tanu chaudhary <kaurtanu213@gmail.com>
2025-06-30 19:58:11 +0530

    working on blog detail page ui

--- src/components/blogdetails/TravelBlog.tsx
+++ src/components/blogdetails/TravelBlog.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import BlogDetailContent from "./BlogDetailContent";
 import TableOfContent from "./TableOfContent";
+import PostCard from "./PostCard";
 
 const TravelBlog = () => {
   return (
-    <section className="main-container flex gap-4 pt-8">
-      <div className="basis-[71%]">
-        <BlogDetailContent />
+    <section className="main-container space-y-20 pt-8">
+      <div className="flex flex-col justify-between gap-4 lg:flex-row">
+        <div className="1xl:basis-[71%] lg:w-2/3">
+          <BlogDetailContent />
+        </div>
+        <div className="1xl:basis-[26%] lg:w-1/3">
+          <TableOfContent />
+        </div>
       </div>
-      <div className="basis-[26%]">
-        <TableOfContent />
-      </div>
+      <PostCard />
     </section>
   );
 };

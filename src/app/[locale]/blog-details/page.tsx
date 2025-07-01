20fd971c4f96775e1496bc429a1f886cfde63b47
Tanu chaudhary <kaurtanu213@gmail.com>
2025-07-01 09:43:37 +0530

    add all content in blog detail

--- src/app/[locale]/blog-details/page.tsx
+++ src/app/[locale]/blog-details/page.tsx
@@ -2,6 +2,7 @@ import BlogDetailHero from "@/components/blogdetails/BlogDetailHero";
 import ContractorArticles from "@/components/blogdetails/ContractorArticles";
 import TravelBlog from "@/components/blogdetails/TravelBlog";
 import { platforms } from "@/components/common/Helper";
+import { FooterRedLineMobileIcon } from "@/components/common/Icons";
 import CrmSercive from "@/components/crmbussiness/CrmSercive";
 import TrustBarHvca from "@/components/industry/hvca/TrustBarHvca";
 import React from "react";
@@ -18,19 +19,22 @@ const BlogDetails = () => {
         <BlogDetailHero />
         <TravelBlog />
       </main>
-      <CrmSercive
-        data={{
-          title: "The only OS for build and service contractors.",
-          placeholder: "Enter your email",
-        }}
-        ncc="No credit card required"
-        createBtn="Get started FREE"
-        mobileBtn="Get Started"
-        showClouds={true}
-        className="w-full max-w-[646px]"
-        variant="primary"
-        variantBtn="dark"
-      />
+      <div className="relative">
+        <CrmSercive
+          data={{
+            title: "The only OS for build and service contractors.",
+            placeholder: "Enter your email",
+          }}
+          ncc="No credit card required"
+          createBtn="Get started FREE"
+          mobileBtn="Get Started"
+          showClouds={true}
+          className="w-full max-w-[646px]"
+          variant="primary"
+          variantBtn="dark"
+        />
+        <FooterRedLineMobileIcon className="pointer-events-none absolute top-[-20%] -left-[1%] block max-h-[994px] w-full max-w-[840px]" />
+      </div>
       <TrustBarHvca platforms={platforms} className="pb-[148px] xl:pb-20" />
       <ContractorArticles />
     </>
--- src/components/blogdetails/ContractorArticles.tsx
+++ src/components/blogdetails/ContractorArticles.tsx
@@ -5,11 +5,20 @@ import BlogCard from "../blog/BlogCard";
 import SwiperNavWithPagination from "../blog/SwiperNavWithPagination";
 import { contractorArticles } from "../common/Helper";
 import { CardArrowIcon } from "../common/MainIcon";
+import CloudsAnimation from "../common/CloudsAnimation";
 
 const ContractorArticles = () => {
   return (
-    <div className="bg-white">
-      <div className="relative z-20 mx-auto mt-8 w-full max-w-[1224px] px-2 pb-6">
+    <div className="relative bg-white">
+      <div className="pointer-events-none absolute -top-[5%] left-0 -z-0 hidden h-24 w-full bg-white blur-[12px] lg:block"></div>
+      <div className="pointer-events-none absolute -top-[13%] left-0 z-20 hidden h-[160px] w-full lg:block">
+        <CloudsAnimation
+          cloud1Class="top-0"
+          cloud2Class="top-[57px] sm:top-[50px] md:top-[55px] lg:top-0"
+        />
+      </div>
+
+      <div className="relative z-20 mx-auto mt-8 w-full max-w-[1224px] px-2 pt-8 pb-6">
         <div className="flex items-center justify-between pb-4">
           <h2 className="text-eerieBlack text-xl font-semibold md:text-2xl lg:pb-6 xl:pb-8">
             Related articles from Contractor+ HQ

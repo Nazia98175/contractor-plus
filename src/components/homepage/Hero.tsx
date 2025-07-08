b92e9c9654df47edf5b06de763a9f2cf87a9e15e
suneel kumar <kumarsuneel162@gmail.com>
2025-07-08 20:32:26 +0530

    testing text animation

--- src/components/homepage/Hero.tsx
+++ src/components/homepage/Hero.tsx
@@ -39,16 +39,16 @@ const Hero = ({ homePageContent }: { homePageContent: any }) => {
       <div className="relative mx-auto w-full max-w-[1920px] overflow-hidden pt-[269px] pb-10 sm:pb-16 md:pb-20 lg:pt-[140px] lg:pb-[140px] xl:pb-[163px]">
         <div className="main-container relative z-10 flex items-end">
           <div className="relative z-30 flex w-full flex-col gap-[6px] sm:gap-6 lg:max-w-[628px]">
-            <TextAnimation delay={0.1}>
+            {/* <TextAnimation delay={0.1}>
               <h1 className="main-heading gradient-text xs:w-full w-full max-w-[85%]">
                 {heroTitle}
               </h1>
-            </TextAnimation>
-            {/* <SplitText
-              splitType="lines"
-              className="main-heading w-full max-w-[300px]"
+            </TextAnimation> */}
+            <SplitText
+              splitType="chars"
+              className="main-heading w-full max-w-[300px] text-white"
               text="dsfghvcdfxgchjbklkkfghjklljhgfhjkl;kjhghjkl;kjhgfhjkl"
-            /> */}
+            />
             <CardReveal distance={50} delay={0.4}>
               <p className="text-decemberSky max-w-[304px] text-xs font-semibold sm:max-w-[87%] sm:text-sm md:text-base md:font-medium lg:text-lg">
                 {heroDescription}

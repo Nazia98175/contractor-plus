bbd7c304224d602f8f81fcd493bb632074ef67af
Tanu chaudhary <kaurtanu213@gmail.com>
2025-07-08 13:45:09 +0530

    working on estimatic hero

--- src/app/[locale]/feature/estimatic-ai/page.tsx
+++ src/app/[locale]/feature/estimatic-ai/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const EstimaticAiPage = () => {
   return (
-    <main>
+    <main className="overflow-hidden">
       <EstimaticHero />
     </main>
   );
---
+++ src/components/estimaticAi/StatisticData.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StatisticData = () => {
+  return <article>StatisticData</article>;
+};
+
+export default StatisticData;

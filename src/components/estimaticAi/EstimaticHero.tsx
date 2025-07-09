2803ef1d0b540301ce68d4c531c04b60e21d8e22
Tanu chaudhary <kaurtanu213@gmail.com>
2025-07-09 13:05:09 +0530

    add stat card

--- src/components/estimaticAi/EstimaticHero.tsx
+++ src/components/estimaticAi/EstimaticHero.tsx
@@ -8,6 +8,24 @@ import CardRequiredButton from "../common/CardRequiredButton";
 import StatisticData from "./StatisticData";
 
 const EstimaticHero = () => {
+  const estimateHeroData = [
+    {
+      title: "<9",
+      desc: "Minutes spent on an estimate",
+    },
+    {
+      title: "$100k+",
+      desc: "More in annual profit from fewer underbids",
+    },
+    {
+      title: "2x",
+      desc: "Higher close rate for estimates sent same-day",
+    },
+    {
+      title: "30%",
+      desc: "Reduction in material cost errors",
+    },
+  ];
   return (
     <section className="bg-cover bg-fixed text-white xl:bg-[url('/images/webp/estimatic-hero-bg.webp')]">
       <div className="main-container flex justify-center pt-[164px] pb-20 xl:justify-between">
@@ -116,7 +134,11 @@ const EstimaticHero = () => {
         </div>
       </div>
 
-      <StatisticData />
+      <div className="main-container grid grid-cols-1 place-items-center gap-5 sm:grid-cols-2 lg:grid-cols-4 xl:gap-8">
+        {estimateHeroData.map((obj, index) => (
+          <StatisticData key={index} obj={obj} />
+        ))}
+      </div>
     </section>
   );
 };
--- src/components/estimaticAi/StatisticData.tsx
+++ src/components/estimaticAi/StatisticData.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 
-const StatisticData = () => {
-  return <article>StatisticData</article>;
+interface StatisticDataProps {
+  obj: {
+    title: string;
+    desc: string;
+  };
+}
+
+const StatisticData: React.FC<StatisticDataProps> = ({ obj }) => {
+  return (
+    <article className="flex w-full max-w-[307px] flex-col">
+      <span className="h-[1px] w-full bg-[radial-gradient(circle,_#ff0000_0%,_rgba(0,0,0,0.6)_60%,_rgba(0,0,0,1)_100%)]"></span>
+      <h4 className="pt-2.5 text-center text-[22px] font-bold lg:text-[32px]">
+        {obj.title}
+      </h4>
+      <p className="py-2.5 text-center text-xs font-medium">{obj.desc}</p>
+    </article>
+  );
 };
 
 export default StatisticData;

cac9764bbcc61ee433c55d8ddd89c5a108d943df
Tanu chaudhary <kaurtanu213@gmail.com>
2025-07-12 19:31:06 +0530

    delete extra images

--- src/components/common/Helper.tsx
+++ src/components/common/Helper.tsx
@@ -3000,7 +3000,7 @@ export const estimaticReviews = {
           {
             id: 1,
             userName: "Richard Tooley",
-            profileUrl: null,
+            profileImg: null,
             rating: 5,
             videoLink: null,
             isModal: false,
@@ -3011,7 +3011,7 @@ export const estimaticReviews = {
           {
             id: 2,
             userName: "Richard Tooley",
-            profileUrl: null,
+            profileImg: null,
             rating: 5,
             videoLink: null,
             isModal: false,
@@ -3022,7 +3022,7 @@ export const estimaticReviews = {
           {
             id: 3,
             userName: "Scott Azure",
-            profileUrl: "/images/webp/rands.webp",
+            profileImg: "/images/webp/rands.webp",
             rating: 5,
             videoLink: "https://www.youtube.com/watch?v=CllT0U-CtGc",
             isModal: true,
@@ -3033,7 +3033,7 @@ export const estimaticReviews = {
           {
             id: 4,
             userName: "Richard Tooley",
-            profileUrl: null,
+            profileImg: null,
             rating: 5,
             videoLink: null,
             isModal: false,
@@ -3044,7 +3044,7 @@ export const estimaticReviews = {
           {
             id: 5,
             userName: "Richard Tooley",
-            profileUrl: null,
+            profileImg: null,
             rating: 5,
             videoLink: null,
             isModal: false,
@@ -3055,7 +3055,7 @@ export const estimaticReviews = {
           {
             id: 6,
             userName: "Scott Azure",
-            profileUrl: "/images/webp/rands.webp",
+            profileImg: "/images/webp/rands.webp",
             rating: 5,
             videoLink: "https://www.youtube.com/watch?v=CllT0U-CtGc",
             isModal: true,

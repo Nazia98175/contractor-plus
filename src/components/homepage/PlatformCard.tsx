c413903c7c34d6f20eac385a2ef9ae0bc8dac9d0
suneel kumar <kumarsuneel162@gmail.com>
2025-07-15 16:43:00 +0530

    update card revel

--- src/components/homepage/PlatformCard.tsx
+++ src/components/homepage/PlatformCard.tsx
@@ -22,17 +22,15 @@ const PlatformCard: React.FC<PlatformCardProps> = ({
   apiData = false, // Default to false if not provided
 }) => {
   return (
-    <div className="plateform-cards relative z-20 flex flex-col items-center justify-between gap-2.5 sm:w-fit lg:w-[47%]">
-      <CardReveal distance={50} delay={0.2}>
-        <Image
-          width={155}
-          height={94}
-          sizes="(max-width: 768px) 155px, (min-width: 769px) 155px"
-          src={(platform.url as string) || platform.logo}
-          alt={`${platform.name} rating`}
-          className={`${className} max-w-[140px] object-contain sm:max-w-[155px]`}
-        />
-      </CardReveal>
+    <CardReveal className="plateform-cards relative z-20 flex flex-col items-center justify-between gap-2.5 sm:w-fit lg:w-[47%]">
+      <Image
+        width={155}
+        height={94}
+        sizes="(max-width: 768px) 155px, (min-width: 769px) 155px"
+        src={(platform.url as string) || platform.logo}
+        alt={`${platform.name} rating`}
+        className={`${className} max-w-[140px] object-contain sm:max-w-[155px]`}
+      />
       {!apiData && (
         <div className="flex gap-0.5">
           {[...Array(5)].map((_, i) => (
@@ -42,7 +40,7 @@ const PlatformCard: React.FC<PlatformCardProps> = ({
           ))}
         </div>
       )}
-    </div>
+    </CardReveal>
   );
 };
 

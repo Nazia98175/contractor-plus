5a1b0505f13217dbdaea3c35f2a2b6a4b7f835a5
innovaft-co <innovaft.co@gmail.com>
2025-07-16 14:33:23 +0530

    conflict

--- src/components/industry/IndustryHero.tsx
+++ src/components/industry/IndustryHero.tsx
@@ -1,11 +1,10 @@
-// Updated IndustryHero component with adaptive title
+
 "use client";
 import CardRequiredButton from "@/components/common/CardRequiredButton";
 import CloudsAnimation from "@/components/common/CloudsAnimation";
 import FreeAccountButton from "@/components/common/FreeAccountButton";
-// import AdaptiveHeroTitle from "@/components/common/AdaptiveHeroTitle";
-import gsap from "gsap";
 import { TheHeroProps } from "@/components/crmbussiness/CrmHero";
+import gsap from "gsap";
 import Image from "next/image";
 import React, { useEffect } from "react";
 import AdaptiveHeroTitle from "./AdaptiveHeroTitle";
@@ -36,9 +35,7 @@ const IndustryHero: React.FC<TheHeroProps> = ({
         });
       }, 700);
     }, []);
-
-  const imageBaseUrl = `${process.env.NEXT_PUBLIC_API_IMAGE_URL_STRAPI as string}`;
-//  console.log(homeCard, "homeCard in IndustryHero");
+    
   return (
     <section className="bg-kuroiBlack relative overflow-hidden">
       <div className="relative mx-auto w-full max-w-[1920px] overflow-hidden">

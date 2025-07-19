24d03ab8f0c37002656787489e1436b6aa184cca
rohit <rohit780790809@gmail.com>
2025-07-19 16:51:25 +0530

    fix count hub

--- src/components/crmbussiness/TeamsUsingContractor.tsx
+++ src/components/crmbussiness/TeamsUsingContractor.tsx
@@ -98,7 +98,7 @@ const TeamsUsingContractor: React.FC<Props> = ({ data, slug }) => {
             width={121}
             height={90}
             src="/images/webp/software-advice.webp"
-            className="drop-shadow-img-shadow isolate max-w-[116px] cursor-pointer duration-300 hover:!scale-105 hover:!rotate-6 sm:max-w-[121px]"
+            className="max-w-[116px] cursor-pointer duration-300 hover:!scale-105 hover:!rotate-6 sm:max-w-[121px]"
             sizes="(max-width: 768px) 40vw, 121px"
             alt="Software Advice"
           />
@@ -108,7 +108,7 @@ const TeamsUsingContractor: React.FC<Props> = ({ data, slug }) => {
             width={121}
             height={90}
             src="/images/webp/leader.webp"
-            className="drop-shadow-img-shadow isolate max-w-[93px] cursor-pointer duration-300 hover:!scale-105 hover:!rotate-6 sm:max-w-[103px]"
+            className="max-w-[93px] cursor-pointer duration-300 hover:!scale-105 hover:!rotate-6 sm:max-w-[103px]"
             alt="Leader"
           />
 
@@ -117,7 +117,7 @@ const TeamsUsingContractor: React.FC<Props> = ({ data, slug }) => {
             width={121}
             height={90}
             src="/images/webp/get-app.webp"
-            className="drop-shadow-img-shadow isolate max-w-[111px] cursor-pointer duration-300 hover:!scale-105 hover:!rotate-6 sm:max-w-[137px]"
+            className="max-w-[111px] cursor-pointer duration-300 hover:!scale-105 hover:!rotate-6 sm:max-w-[137px]"
             alt="Get App"
             sizes="(max-width: 768px) 40vw, 121px"
           />
@@ -127,7 +127,7 @@ const TeamsUsingContractor: React.FC<Props> = ({ data, slug }) => {
             width={121}
             height={90}
             src="/images/svg/capterra.svg"
-            className="drop-shadow-img-shadow isolate cursor-pointer duration-300 hover:!scale-105 hover:!rotate-6"
+            className="cursor-pointer duration-300 hover:!scale-105 hover:!rotate-6"
             alt="Capterra"
             sizes="(max-width: 768px) 40vw, 121px"
           />

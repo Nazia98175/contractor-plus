a0202197c232c392267bdecec6b4afbde135bb50
suneel kumar <kumarsuneel162@gmail.com>
2025-07-21 14:43:36 +0530

    update path

--- src/components/common/EventsCard.tsx
+++ src/components/common/EventsCard.tsx
@@ -18,10 +18,10 @@ interface EventsCardProps {
 
 const EventsCard: React.FC<EventsCardProps> = ({ Item }) => {
   return (
-    <article>
+    <article className="group cursor-pointer">
       <CardReveal delay={0.1} distance={50}>
         <Image
-          className="h-full max-h-[240px] w-full rounded-lg object-cover"
+          className="h-full max-h-[240px] w-full rounded-lg object-cover duration-200 ease-in-out group-hover:scale-95"
           src={Item.imgPath}
           alt={Item.heading}
           width={384}
@@ -29,27 +29,31 @@ const EventsCard: React.FC<EventsCardProps> = ({ Item }) => {
         />
       </CardReveal>
       <Copy delay={0.2}>
-        <p className="text-secondary mt-4 text-xs font-semibold lg:mt-6 lg:text-sm xl:mt-8">
+        <p className="text-secondary mt-4 text-xs font-semibold duration-200 ease-in-out group-hover:!text-white lg:mt-6 lg:text-sm xl:mt-8">
           {Item.role}
         </p>
       </Copy>
       <CardReveal delay={0.3} distance={50}>
         <div className="my-3 flex items-center justify-between gap-4">
-          <h2 className="event-card-tittle">{Item.heading}</h2>
+          <h2 className="event-card-tittle duration-200 ease-in-out group-hover:!text-white">
+            {Item.heading}
+          </h2>
           <CardArrowIcon />
         </div>
       </CardReveal>
       <Copy delay={0.4}>
-        <h3 className="text-flintstone text-sm lg:text-base">
+        <h3 className="text-flintstone text-sm duration-200 ease-in-out group-hover:!text-white lg:text-base">
           {Item.description}
         </h3>
       </Copy>
-      <CardReveal delay={0.4} distance={50}>
-        <i className="font-montserrat mt-4 flex items-center gap-1 px-2 py-1.5 text-xs leading-[142.857%] font-extrabold tracking-[0.1px] text-white lg:text-sm xl:mt-6 xl:px-3">
-          {Item.linkPath}
-          <EventDetailIcon />
-        </i>
-      </CardReveal>
+      <Copy delay={0.5}>
+        <div className="font-montserrat mt-4 px-2 py-1.5 text-xs leading-[142.857%] font-extrabold tracking-[0.1px] text-white lg:text-sm xl:mt-6 xl:px-3">
+          <i className="!flex items-center gap-1">
+            {Item.linkPath}
+            <EventDetailIcon />
+          </i>
+        </div>
+      </Copy>
     </article>
   );
 };
--- src/components/events-directory/ConferenceCard.tsx
+++ src/components/events-directory/ConferenceCard.tsx
@@ -23,80 +23,97 @@ const ConferenceCard = ({
   swiperId,
 }: ConferenceCardProps) => {
   return (
-    <section className="main-container">
-      <div className="custom-pagination-2 mb-[33px] flex flex-col items-center justify-between gap-4 sm:flex-row">
+    <>
+      <div className="flex justify-between sm:hidden">
         <Copy delay={0.1}>
           <h4 className="event-card-tittle">{sectionHeading}</h4>
         </Copy>
-        <div className="flex items-center gap-5">
-          <div className="relative flex items-center gap-2">
-            <CardReveal delay={0.1} distance={50}>
-              <button
-                className={`swiper-button-prev-${swiperId} relative flex h-6 w-6 rotate-180 items-center justify-center opacity-100 disabled:opacity-40`}
+        <Copy delay={0.1}>
+          <Link
+            className="font-montserrat text-sm leading-[142.857%] font-medium tracking-[0.1px] whitespace-nowrap text-white"
+            href="/"
+          >
+            View All
+          </Link>
+        </Copy>
+      </div>
+      <section className="main-container flex flex-col-reverse gap-6 sm:flex-col sm:gap-0">
+        <div className="custom-pagination-2 mb-[33px] flex flex-col items-center justify-between gap-4 sm:flex-row">
+          <Copy delay={0.1}>
+            <h4 className="event-card-tittle !hidden sm:!block">
+              {sectionHeading}
+            </h4>
+          </Copy>
+          <div className="mx-auto flex w-fit items-center justify-between gap-5 sm:mx-0">
+            <div className="relative flex items-center gap-2">
+              <CardReveal delay={0.1} distance={50}>
+                <button
+                  className={`swiper-button-prev-${swiperId} relative flex h-6 w-6 rotate-180 items-center justify-center opacity-100 disabled:opacity-40`}
+                >
+                  <CustomSliderIcon />
+                </button>
+              </CardReveal>
+              <CardReveal delay={0.2} distance={50}>
+                <div
+                  className={`swiper-pagination-${swiperId} swiper-pagination-real-time-4 relative left-0 flex items-center justify-center gap-1`}
+                />
+              </CardReveal>
+              <CardReveal delay={0.3} distance={50}>
+                <button
+                  className={`swiper-button-next-${swiperId} relative flex h-6 w-6 items-center justify-center opacity-100 disabled:opacity-40`}
+                >
+                  <CustomSliderIcon />
+                </button>
+              </CardReveal>
+            </div>
+            <Copy delay={0.4}>
+              <Link
+                className="font-montserrat hidden text-sm leading-[142.857%] font-medium tracking-[0.1px] text-white sm:flex"
+                href="/"
               >
-                <CustomSliderIcon />
-              </button>
-            </CardReveal>
-            <CardReveal delay={0.2} distance={50}>
-              <div
-                className={`swiper-pagination-${swiperId} swiper-pagination-real-time-4 relative left-0 flex items-center justify-center gap-1`}
-              />
-            </CardReveal>
-            <CardReveal delay={0.3} distance={50}>
-              <button
-                className={`swiper-button-next-${swiperId} relative flex h-6 w-6 items-center justify-center opacity-100 disabled:opacity-40`}
-              >
-                <CustomSliderIcon />
-              </button>
-            </CardReveal>
+                View All
+              </Link>
+            </Copy>
           </div>
-          <CardReveal delay={0.4} distance={50}>
-            <Link
-              className="font-montserrat text-sm leading-[142.857%] font-medium tracking-[0.1px] text-white"
-              href="/"
-            >
-              View All
-            </Link>
-          </CardReveal>
         </div>
-      </div>
-      <div className="gap-8">
-        <Swiper
-          modules={[Pagination, Navigation]}
-          pagination={{
-            el: `.swiper-pagination-${swiperId}`,
-            clickable: true,
-            dynamicBullets: true,
-          }}
-          navigation={{
-            nextEl: `.swiper-button-next-${swiperId}`,
-            prevEl: `.swiper-button-prev-${swiperId}`,
-          }}
-          spaceBetween={10}
-          slidesPerView={1}
-          breakpoints={{
-            600: {
-              slidesPerView: 2,
-              spaceBetween: 20,
-            },
-            900: {
-              slidesPerView: 2,
-              spaceBetween: 20,
-            },
-            1024: {
-              slidesPerView: 3,
-              spaceBetween: 32,
-            },
-          }}
-        >
-          {EventCardItem.map((Item: any, index: Key | null | undefined) => (
-            <SwiperSlide key={index}>
-              <EventsCard Item={Item} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-    </section>
+        <div className="gap-8">
+          <Swiper
+            modules={[Pagination, Navigation]}
+            pagination={{
+              el: `.swiper-pagination-${swiperId}`,
+              clickable: true,
+              dynamicBullets: true,
+            }}
+            navigation={{
+              nextEl: `.swiper-button-next-${swiperId}`,
+              prevEl: `.swiper-button-prev-${swiperId}`,
+            }}
+            spaceBetween={10}
+            slidesPerView={1}
+            breakpoints={{
+              600: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+              },
+              900: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+              },
+              1024: {
+                slidesPerView: 3,
+                spaceBetween: 32,
+              },
+            }}
+          >
+            {EventCardItem.map((Item: any, index: Key | null | undefined) => (
+              <SwiperSlide key={index}>
+                <EventsCard Item={Item} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </section>
+    </>
   );
 };
 

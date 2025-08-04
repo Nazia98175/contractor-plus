e4d51e0b20fa25513a4258bea18175873ec7647f
Kanchan Jadon <kanchan.jadon@snva.com>
2025-08-04 21:52:57 +0530

    not found redirect to home

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,6 +3,7 @@
 import Button from "@/components/common/Button";
 import gsap from "gsap";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function NotFound() {
@@ -23,6 +24,7 @@ export default function NotFound() {
       });
     }, 700);
   }, []);
+  const router = useRouter();
   return (
     <section id="home-page-wrapper-2">
       <div
@@ -46,7 +48,7 @@ export default function NotFound() {
           <Button
             className="!w-fit"
             type="button"
-            onClick={() => window.location.reload()}
+            onClick={() => router.push('/')}
           >
             Back To Homepage
           </Button>

0df2cff45748cee7c4a504b1bb8a50d9e04e3a0b
suneel kumar <kumarsuneel162@gmail.com>
2025-08-06 10:06:21 +0530

    update code

--- src/components/common/FeaturesDropdown.tsx
+++ src/components/common/FeaturesDropdown.tsx
@@ -128,7 +128,7 @@ const FeaturesDropdown: React.FC<Props> = ({
           ))}
       </div>
       <div className="font-inter bg-doctor2 sticky bottom-0 left-0 flex w-full items-center justify-between gap-6">
-        <Link className="all-features-button group" href="/">
+        <Link className="all-features-button group" href={"/"}>
           {headerSubList[headerSubList?.length - 1]?.links?.[0]?.linkTxt}
           <ArrowIcon />
         </Link>

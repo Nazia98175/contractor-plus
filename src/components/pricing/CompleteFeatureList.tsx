72f56e275a8fc9bf352cf0fc287eff261ac97d6d
Tanu chaudhary <kaurtanu213@gmail.com>
2025-08-07 11:31:52 +0530

    remove : console

--- src/components/pricing/CompleteFeatureList.tsx
+++ src/components/pricing/CompleteFeatureList.tsx
@@ -2,7 +2,6 @@
 import { PlansProps, Review } from "@/types";
 import React, { useState } from "react";
 import Marquee from "react-fast-marquee";
-import { reviews } from "../common/Helper";
 import { ScrollDownIcon } from "../common/Icons";
 import ReviewCard from "../common/ReviewCard";
 import ReviewModal from "../common/ReviewModal";
@@ -16,7 +15,6 @@ const CompleteFeatureList: React.FC<PlansProps> = ({ onScroll, reviews }) => {
     setIsModalOpen(true);
   };
 
-  console.log(reviews, "reviews");
   return (
     <section className="relative mx-auto flex w-full max-w-[1920px] flex-col items-center pt-2.5 lg:pt-2">
       <div className="pointer-events-none absolute -top-[20%] left-1/2 z-10 hidden h-[160%] w-[116%] -translate-x-1/2 rounded-[1805px] border-[160px] border-white bg-transparent blur-[55px] xl:block"></div>

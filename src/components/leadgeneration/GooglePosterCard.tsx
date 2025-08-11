0a6901d49366be27f7e1205827a705e633b7f762
VividlyMethod <vividly.fyi@gmail.com>
2025-08-11 19:23:36 +0530

    extra code removed

--- src/components/leadgeneration/GooglePosterCard.tsx
+++ src/components/leadgeneration/GooglePosterCard.tsx
@@ -191,39 +191,37 @@ const GooglePosterCard: React.FC = () => {
       </div>
 
       {/* THIRD-CARD */}
-      <div id="card3-wrapper" style={{ willChange: "transform" }}>
-        <div
-          ref={card3Ref}
-          style={{ willChange: "transform" }}
-          className="flex flex-col gap-1 rounded-xl bg-white p-2.5"
-        >
-          <div className="flex items-center justify-between gap-2">
-            <h3 className="font-inter text-palatinate mb-0.5 text-xs font-semibold">
-              Your Contracting Biz
-            </h3>
-            <div
-              id="seo-card-3"
-              className="bg-silver text-pantone3 flex h-5 min-h-5 w-5 min-w-5 items-center justify-center rounded-full text-[10px] font-extrabold tracking-[-0.2px]"
-            >
-              {cardRanking.card3}
-            </div>
+      <div
+        ref={card3Ref}
+        style={{ willChange: "transform" }}
+        className="flex flex-col gap-1 rounded-xl bg-white p-2.5"
+      >
+        <div className="flex items-center justify-between gap-2">
+          <h3 className="font-inter text-palatinate mb-0.5 text-xs font-semibold">
+            Your Contracting Biz
+          </h3>
+          <div
+            id="seo-card-3"
+            className="bg-silver text-pantone3 flex h-5 min-h-5 w-5 min-w-5 items-center justify-center rounded-full text-[10px] font-extrabold tracking-[-0.2px]"
+          >
+            {cardRanking.card3}
           </div>
-          <p className="font-inter text-boogie text-[10px] font-semibold">
-            www.yourbusiness.com
-          </p>
-          <span className="font-inter text-mana text-[10px] font-semibold">
-            Top-rated general contractor serving CityName Indianapolis and
-            surrounding areas. Licensed, insured, and trusted by homeowners for
-            quality renovations and custom builds.
-          </span>
-          <div className="font-inter text-mana flex items-center text-[10px] font-semibold">
-            <SmallStarIcon />
-            <SmallStarIcon />
-            <SmallStarIcon />
-            <SmallStarIcon />
-            <SmallStarIcon />
-            (1,415)
-          </div>
+        </div>
+        <p className="font-inter text-boogie text-[10px] font-semibold">
+          www.yourbusiness.com
+        </p>
+        <span className="font-inter text-mana text-[10px] font-semibold">
+          Top-rated general contractor serving CityName Indianapolis and
+          surrounding areas. Licensed, insured, and trusted by homeowners for
+          quality renovations and custom builds.
+        </span>
+        <div className="font-inter text-mana flex items-center text-[10px] font-semibold">
+          <SmallStarIcon />
+          <SmallStarIcon />
+          <SmallStarIcon />
+          <SmallStarIcon />
+          <SmallStarIcon />
+          (1,415)
         </div>
       </div>
     </div>

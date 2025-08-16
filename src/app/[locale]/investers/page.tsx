9c4c5e5cb19550be188fa604bc726a8e4cd1801f
rohit <rohit780790809@gmail.com>
2025-08-16 18:24:23 +0530

    new page added

---
+++ src/app/[locale]/investers/page.tsx
@@ -0,0 +1,12 @@
+import TheProblem from "@/components/investers/TheProblem";
+import React from "react";
+
+const InversterPage = () => {
+  return (
+    <div>
+      <TheProblem />
+    </div>
+  );
+};
+
+export default InversterPage;
---
+++ src/components/investers/InvestersHero.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InvestersHero = () => {
+  return (
+    <div>InvestersHero</div>
+  )
+}
+
+export default InvestersHero

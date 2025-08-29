af4dafa3532363af40aa3483ceb5a926a2a1db9c
innovaft-co <innovaft.co@gmail.com>
2025-08-29 16:58:07 +0530

    card spacing

--- src/components/common/EventsCard.tsx
+++ src/components/common/EventsCard.tsx
@@ -31,12 +31,12 @@ const EventsCard: React.FC<EventsCardProps> = ({ Item, onClick }) => {
         />
       </CardReveal>
       <Copy delay={0.2}>
-        <p className="text-secondary mt-4 text-xs font-semibold duration-200 ease-in-out lg:mt-6 lg:text-sm xl:mt-8">
+        <p className="text-secondary mt-4 text-xs font-semibold duration-200 ease-in-out lg:mt-6 lg:text-sm xl:mt-8 px-3">
           {Item.role}
         </p>
       </Copy>
       <CardReveal delay={0.3} distance={50}>
-        <div className="my-3 flex items-center justify-between gap-4">
+        <div className="my-3 flex items-center justify-between gap-4 px-3">
           <h2 className="event-card-tittle duration-200 ease-in-out">
             {Item.heading}
           </h2>
@@ -44,12 +44,12 @@ const EventsCard: React.FC<EventsCardProps> = ({ Item, onClick }) => {
         </div>
       </CardReveal>
       <Copy delay={0.4}>
-        <h3 className="text-flintstone text-sm duration-200 ease-in-out lg:text-base">
+        <h3 className="text-flintstone text-sm duration-200 ease-in-out lg:text-base px-3">
           {Item.description}
         </h3>
       </Copy>
       <Copy delay={0.5}>
-        <div className="font-montserrat mt-4 px-2 py-1.5 text-xs leading-[142.857%] font-extrabold tracking-[0.1px] text-white lg:text-sm xl:mt-6 xl:px-3">
+        <div className="font-montserrat mt-4 px-2 py-1.5 text-xs leading-[142.857%] font-extrabold tracking-[0.1px] text-white lg:text-sm xl:my-4 xl:px-3">
           <i className="!flex items-center gap-1">
             {Item.linkPath}
             <EventDetailIcon />

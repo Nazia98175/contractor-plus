797a0aa8f0826f7cc6420f6e9b790b9c11f4fd86
innovaft-co <innovaft.co@gmail.com>
2025-08-30 16:14:48 +0530

    test image added

--- src/app/[locale]/[slug]/page.tsx
+++ src/app/[locale]/[slug]/page.tsx
@@ -98,6 +98,10 @@ const CrmBussinessPage = async ({ params }: CrmBussinessPageProps) => {
     mobileBtn: commonData?.mobileBtn,
     ncc: commonData?.nccTxt,
   };
+
+
+  console.log(heroImg,"heroImg");
+  
   return (
     <>
       <div id="home-page-wrapper-2">

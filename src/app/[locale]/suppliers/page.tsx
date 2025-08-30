a11f7e3cadf5605bdfd77bafcaa28a6cc544a4ba
sunil <kumarsuneel162@gmail.com>
2025-08-30 15:46:34 +0530

    update og

--- src/app/[locale]/suppliers/page.tsx
+++ src/app/[locale]/suppliers/page.tsx
@@ -13,9 +13,22 @@ import React from "react";
 export const metadata = {
   title: "Suppliers: Connect with Thousands of Contractors",
   description:
-    "List your products or services and reach verified contractors using the Contractor+ network.",
+    "List your products or services and reach verified contractors using the Contractor+ network. ",
+  keywords: ["Opportunity Tracker for Contractors | Contractor+"],
+  openGraph: {
+    images: [
+      {
+        url: "/images/webp/supplier-og.webp",
+        width: 1920,
+        height: 630,
+        alt: "supplier-og-og",
+      },
+    ],
+  },
+  alternates: {
+    canonical: "https://v2site.contractorplus.app/suppliers",
+  },
 };
-
 const Supplierspage = () => {
   return (
     <div className="overflow-hidden">

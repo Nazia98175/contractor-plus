15eac6d1859f212d09ab5ad20596f6f62557f76a
sunil <kumarsuneel162@gmail.com>
2025-09-19 12:57:56 +0530

    fix img

--- src/app/[locale]/[slug]/page.tsx
+++ src/app/[locale]/[slug]/page.tsx
@@ -267,7 +267,8 @@ const CrmBussinessPage = async ({ params }: CrmBussinessPageProps) => {
               )}
               {useParams.slug !== "mileage-tracking" &&
                 useParams.slug !== "contractor-client-agreemenat" &&
-                useParams.slug !== "tool-inventory-software" && (
+                useParams.slug !== "tool-inventory-software" &&
+                useParams.slug !== "contractor-financing" && (
                   <HowContractorWork
                     ncc={pageData.ncc}
                     trackProperties={pageData.trackProperties}

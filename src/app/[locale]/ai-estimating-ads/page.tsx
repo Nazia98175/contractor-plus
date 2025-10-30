574b8519d8e10fcc8a20109f22d7c7889e5e5f82
rohit <rohit780790809@gmail.com>
2025-10-30 18:12:14 +0530

    fix issues by client

---
+++ src/app/[locale]/ai-estimating-ads/page.tsx
@@ -0,0 +1,110 @@
+import EstimaticHero from "@/components/estimaticAi/EstimaticHero";
+import React from "react";
+
+const page = () => {
+  return (
+    <main id="common-homepage-wrapper">
+      {/* <div
+        id="home-page-view-port-screen"
+        className="relative overflow-hidden opacity-0"
+      > */}
+      {/* <EstimaticHero
+          hero={pageContent}
+          heroImg={heroImg?.url}
+          createBtn={commonData?.getStartedFreeBtn}
+          createMobileBtn={commonData?.mobileBtn}
+          nccTxt={commonData?.nccTxt}
+          estimateHeroData={pageContent?.resultStatsEstimatic || []}
+        /> */}
+      {/* <TrustedService
+          reviews={reviews || []}
+          className="pb-6 lg:pt-6 lg:pb-3.5"
+        />
+        <RunWithContractor
+          kindAdorable={comparisonList}
+          variant="dark"
+          icon={true}
+          issubHeadingShow={true}
+        />
+      </div>
+      <OneGetsSet content={comparisonList?.comparison} />
+      <div className="relative hidden md:block">
+        <FieldService
+          fieldService={problemSolution}
+          theme="dark"
+          mainClassName="text-center "
+        />
+      </div>
+      <div className="mb-12 block md:hidden">
+        <OverlapCardMobileViewChild
+          fieldService={problemSolution}
+          theme="dark"
+          mainClassName="text-center "
+        />
+      </div>
+      <div className="relative overflow-hidden">
+        <SwitchingTool className="pb-[113px]" switchingTool={commonProblem} />
+        <ContractorIndustry
+          contractorIndustry={industriesData?.data?.Industries}
+        />
+        <Image
+          className="pointer-events-none absolute bottom-0 left-0 z-20 hidden h-[90%] w-full max-w-[900px] object-center md:block"
+          src="/images/webp/josh-lesson-left-background.webp"
+          alt="webp bg"
+          width={900}
+          height={700}
+        />
+        <Image
+          className="svgTwinkle absolute right-0 bottom-0 z-0 hidden h-[50%] w-full max-w-[700px] object-center lg:block"
+          src="/images/webp/josh-lesson-right-background.webp"
+          alt="webp bg"
+          width={700}
+          height={300}
+        />
+      </div>
+      <AwardsTagsImg className="sm:mt-14" />
+      <ThousandsReviews
+        data={thousandReviews}
+        reviews={thousandReviews?.reviews}
+        variant="secondary"
+      />
+      <div className="pt-20 pb-10 sm:pb-[75px] lg:pt-[110px] xl:pt-[120px]">
+        <CommonFormField
+          variantBtn="primary"
+          variant="default"
+          title={
+            emailSignupSection?.title ||
+            "The AI estimate generator that will change your business forever"
+          }
+          subTitle={
+            emailSignupSection?.subTitle ||
+            "Get started with Estimatic AI in Contractor+ today."
+          }
+          placeholder={emailSignupSection?.placeholder || "Your Email"}
+          createBtn={commonData?.getStartedFreeBtn || "Get Started Free"}
+          mobileBtn={commonData?.mobileBtn || "Download FREE App"}
+          ncc={commonData?.nccTxt || "No credit card required"}
+        />
+      </div>
+      <TrustBar
+        platforms={platforms}
+        className="mx-auto w-full max-w-[889px]"
+      />
+      <div className="relative overflow-hidden">
+        <Faq
+          faq={faqs}
+          classNameAnswer="pt-1"
+          mainContainerclassName="px-2 md:pt-[76px] pt-[66px] md:pb-[83px] pb-5 sm:pb-10"
+          TittleClassName="max-w-[82%] xs:max-w-[81%] sm:max-w-full mx-auto"
+        />
+      </div>
+      <BlogPosts
+        data={blogs?.data || []}
+        blogs={pageContent?.blogs}
+        className="pb-8 sm:pb-12 md:mt-9 md:pb-16 lg:pb-20 xl:pb-[99px]"
+      /> */}
+    </main>
+  );
+};
+
+export default page;
